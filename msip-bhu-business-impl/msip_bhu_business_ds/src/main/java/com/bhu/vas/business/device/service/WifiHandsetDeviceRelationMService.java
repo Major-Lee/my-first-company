5ca33df5a1fd0ff142233cad135744866b2a1e55
唐子超 <56755166@qq.com>
2015-03-17 16:29:27 +0800

    added

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/device/service/WifiHandsetDeviceRelationMService.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/device/service/WifiHandsetDeviceRelationMService.java
@@ -8,45 +8,70 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import com.bhu.vas.business.device.dao.WifiHandsetDeviceRelationMDao;
 import com.bhu.vas.business.device.mdto.WifiHandsetDeviceRelationMDTO;
-import com.bhu.vas.business.msequence.mdao.SequenceMDao;
 import com.smartwork.msip.cores.cache.relationcache.impl.springmongo.Pagination;
 
+/**
+ * 移动设备接入wifi设备的接入记录 (mongodb)
+ * 一个移动设备接入同一个wifi设备多次，只有一条记录
+ * id = wifiId_handsetId (无流水id)
+ * @author tangzichao
+ *
+ */
 @Service
 public class WifiHandsetDeviceRelationMService {
 	
 	@Resource
 	private WifiHandsetDeviceRelationMDao wifiHandsetDeviceRelationMDao;
 	
-	@Resource
-	private SequenceMDao sequenceMDao;
+//	@Resource
+//	private SequenceMDao sequenceMDao;
 	
-	public void addRelation(String handsetId, String wifiId, Date login_at){
-		if(StringUtils.isEmpty(handsetId) || StringUtils.isEmpty(wifiId)) return;
-		if(login_at == null) login_at = new Date();
+//	public void addRelation(String handsetId, String wifiId, Date login_at){
+//		if(StringUtils.isEmpty(handsetId) || StringUtils.isEmpty(wifiId)) return;
+//		if(login_at == null) login_at = new Date();
+//		
+//		WifiHandsetDeviceRelationMDTO mdto = new WifiHandsetDeviceRelationMDTO();
+//		mdto.setId(sequenceMDao.getNextSequenceId(WifiHandsetDeviceRelationMDTO.class.getName()));
+//		mdto.setHandsetId(handsetId);
+//		mdto.setWifiId(wifiId);
+//		mdto.setLogin_at(login_at);
+//		mdto = wifiHandsetDeviceRelationMDao.save(mdto);
+//	}
+	
+	public void addRelation(String wifiId, String handsetId, Date last_login_at){
+		if(StringUtils.isEmpty(wifiId) || StringUtils.isEmpty(handsetId)) return;
+		if(last_login_at == null) last_login_at = new Date();
 		
-		WifiHandsetDeviceRelationMDTO mdto = new WifiHandsetDeviceRelationMDTO();
-		mdto.setId(sequenceMDao.getNextSequenceId(WifiHandsetDeviceRelationMDTO.class.getName()));
-		mdto.setHandsetId(handsetId);
-		mdto.setWifiId(wifiId);
-		mdto.setLogin_at(login_at);
-		mdto = wifiHandsetDeviceRelationMDao.save(mdto);
+		WifiHandsetDeviceRelationMDTO mdto = new WifiHandsetDeviceRelationMDTO(wifiId, handsetId);
+		mdto.setLast_login_at(last_login_at);
+		
+		Query query = new Query(Criteria.where("_id").is(mdto.getId()));
+		Update update = Update.update("last_login_at", last_login_at);
+		wifiHandsetDeviceRelationMDao.upsert(query, update);
 	}
 	
-	public Pagination<WifiHandsetDeviceRelationMDTO> findRelations(String wifiId, int pageNo, int pageSize){
+	public Pagination<WifiHandsetDeviceRelationMDTO> findRelationsByWifiId(String wifiId, int pageNo, int pageSize){
 		Query query = new Query(Criteria.where("wifiId").is(wifiId));
-		query.with(new Sort(Direction.DESC,"_id"));
+		query.with(new Sort(Direction.DESC,"last_login_at"));
 		return wifiHandsetDeviceRelationMDao.findPagination(pageNo, pageSize, query);
 	}
 	
-	public Pagination<WifiHandsetDeviceRelationMDTO> findRelations(String wifiId, String handsetId, int pageNo, int pageSize){
-		Query query = new Query(Criteria.where("wifiId").is(wifiId).and("handsetId").is(handsetId));
-		query.with(new Sort(Direction.DESC,"_id"));
+	public Pagination<WifiHandsetDeviceRelationMDTO> findRelationsByHandsetId(String handsetId, int pageNo, int pageSize){
+		Query query = new Query(Criteria.where("handsetId").is(handsetId));
+		query.with(new Sort(Direction.DESC,"last_login_at"));
 		return wifiHandsetDeviceRelationMDao.findPagination(pageNo, pageSize, query);
 	}
+	
+//	public Pagination<WifiHandsetDeviceRelationMDTO> findRelations(String wifiId, String handsetId, int pageNo, int pageSize){
+//		Query query = new Query(Criteria.where("wifiId").is(wifiId).and("handsetId").is(handsetId));
+//		query.with(new Sort(Direction.DESC,"_id"));
+//		return wifiHandsetDeviceRelationMDao.findPagination(pageNo, pageSize, query);
+//	}
 	
 }
