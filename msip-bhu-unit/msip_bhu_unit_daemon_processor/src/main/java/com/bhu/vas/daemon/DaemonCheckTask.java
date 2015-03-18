667a61be952b82bfb2b2a6f0e208d5f9fa89395d
Edmond Lee <wenhualee@163.com>
2015-03-18 12:23:46 +0800

    param 0000和00000006 =》0000和00000000

--- msip-bhu-unit/msip_bhu_unit_daemon_processor/src/main/java/com/bhu/vas/daemon/DaemonCheckTask.java
+++ msip-bhu-unit/msip_bhu_unit_daemon_processor/src/main/java/com/bhu/vas/daemon/DaemonCheckTask.java
@@ -12,14 +12,14 @@ import com.smartwork.msip.localunit.RandomData;
                     设备报文主类型(4字节)，子类型(8字节)
                     后接xml格式的任务字符串.
                     任务id为0，表明任务不需要回复。
- * <param>开头的那个报文，这两个数字是0000和00000006
+ * <param>开头的那个报文，这两个数字是0000和00000006 但先改成0000和00000000，为了调试
  * <cmd>之类的，是0001和00000001
  * @author Edmond
  *
  */
 public class DaemonCheckTask extends TimerTask{
 	//1. 查询当前在线终端，下发此命令后触发设备主动上报一次
-	private String query_device_teminals_cmd_template = "00001001%s%s"+"000000000006"+"<param><ITEM wlan_user_notify=\"enable\" trap=\"disable\" wlan_user_sync=\"1\" /></param>";
+	private String query_device_teminals_cmd_template = "00001001%s%s"+"000000000000"+"<param><ITEM wlan_user_notify=\"enable\" trap=\"disable\" wlan_user_sync=\"1\" /></param>";
 	
 	//1. 查询cpu,内存利用率
 	private String query_device_status_cmd_template =   "00001001%s%s"+"000100000001"+"<cmd><ITEM index=\"1\" cmd=\"sysperf\"/></cmd>";
