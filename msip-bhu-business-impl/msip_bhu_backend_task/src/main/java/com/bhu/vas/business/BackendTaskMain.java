506cd2bf71c54cf68f450db8ec2ee1d6a989cf5d
Edmond Lee <wenhualee@163.com>
2015-03-20 15:50:07 +0800

    added

---
+++ msip-bhu-business-impl/msip_bhu_backend_task/src/main/java/com/bhu/vas/business/BackendTaskMain.java
@@ -0,0 +1,15 @@
+package com.bhu.vas.business;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+
+public class BackendTaskMain {
+	public static void main(String[] args) throws InterruptedException {
+		//String[] locations = {"classpath*:/springtest/testCtx.xml"};//,"classpath:springmq/applicationContext-activemq-server.xml", "classpath:springmq/applicationContext-activemq-message-consumer.xml"};
+		//org.springframework.scheduling.quartz
+		String[] locations = {"classpath*:/spring/appCtx.xml"};
+		@SuppressWarnings("unused")
+		ApplicationContext ctx = new FileSystemXmlApplicationContext(locations);//("classpath*:/springtest/testCtx.xml");//"classpath*:springfeed/applicationContext-activemq-consumer.xml");//"classpath:springtest/testCtx.xml");
+	}
+}
