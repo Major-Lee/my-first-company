26bbc6bbd3459aa9962753d6d57fc58388987061
唐子超 <56755166@qq.com>
2015-04-01 18:52:46 +0800

    added

--- msip-bhu-business-impl/msip_bhu_business_ds/src/test/java/com/bhu/vas/business/mdevices/WifiHandsetDeviceLoginCountMServiceTest.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/test/java/com/bhu/vas/business/mdevices/WifiHandsetDeviceLoginCountMServiceTest.java
@@ -13,13 +13,13 @@ public class WifiHandsetDeviceLoginCountMServiceTest extends BaseTest{
 	@Resource
 	private WifiHandsetDeviceLoginCountMService wifiHandsetDeviceLoginCountMService;
 	
-	//@Test
+	@Test
 	public void testIncr(){
 		wifiHandsetDeviceLoginCountMService.incrCount("w1",4);
 	}
 	
 	
-	@Test
+	//@Test
 	public void testFind(){
 		Pagination<WifiHandsetDeviceLoginCountMDTO> result = wifiHandsetDeviceLoginCountMService.findWifiDevicesOrderMaxHandset(1, 10);
 		System.out.println(result.getTotalCount());
