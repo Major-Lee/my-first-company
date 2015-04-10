7ea94dd832ebb893325dfefaea231d80598ab890
Edmond Lee <wenhualee@163.com>
2015-04-10 20:06:01 +0800

    added

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/RpcResponseDTOBuilder.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/RpcResponseDTOBuilder.java
@@ -3,7 +3,7 @@ package com.bhu.vas.api.rpc;
 import com.smartwork.msip.jdo.ResponseErrorCode;
 
 public class RpcResponseDTOBuilder {
-	public static <T extends java.io.Serializable> RpcResponseDTO<T> builderErrorRpcResponse(ResponseErrorCode errorCode){//,Class<T> classz){
+	public static <T> RpcResponseDTO<T> builderErrorRpcResponse(ResponseErrorCode errorCode){//,Class<T> classz){
 		/*RpcResponseDTO<T> res = new RpcResponseDTO<T>();
 		res.setErrorCode(errorCode);
 		res.setPayload(null);
@@ -11,7 +11,7 @@ public class RpcResponseDTOBuilder {
 		return builderErrorRpcResponse(errorCode,null);
 	}
 	
-	public static <T extends java.io.Serializable> RpcResponseDTO<T> builderErrorRpcResponse(ResponseErrorCode errorCode,T payload){//,Class<T> classz){
+	public static <T> RpcResponseDTO<T> builderErrorRpcResponse(ResponseErrorCode errorCode,T payload){//,Class<T> classz){
 		RpcResponseDTO<T> res = new RpcResponseDTO<T>();
 		res.setErrorCode(errorCode);
 		res.setPayload(payload);
