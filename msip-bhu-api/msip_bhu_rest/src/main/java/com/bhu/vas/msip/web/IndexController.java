e20ab61d246e50db29f29f292cc558b7e4998536
Edmond Lee <wenhualee@163.com>
2015-04-23 14:04:28 +0800

    added

--- msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/msip/web/IndexController.java
+++ msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/msip/web/IndexController.java
@@ -1,14 +1,13 @@
 package com.bhu.vas.msip.web;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bhu.vas.msip.cores.web.mvc.spring.BaseController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class IndexController extends BaseController{
