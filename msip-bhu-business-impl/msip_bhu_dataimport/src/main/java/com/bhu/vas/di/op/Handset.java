24610c17716b1ba90f9cda0686b7fe0a13c39227
Edmond Lee <wenhualee@163.com>
2015-05-15 16:38:33 +0800

    added

---
+++ msip-bhu-business-impl/msip_bhu_dataimport/src/main/java/com/bhu/vas/di/op/Handset.java
@@ -0,0 +1,20 @@
+package com.bhu.vas.di.op;
+
+public class Handset {
+	public enum Android{
+		
+	}
+	
+	public enum iOS{
+		iPhone(),
+		iPad(),
+		iPod(),
+		iMac(),
+		;
+		
+	}
+	
+	public class Windows{
+		
+	}
+}
--- msip-bhu-business-impl/msip_bhu_dataimport/src/main/java/com/bhu/vas/di/op/UserAccessStatisticAnaOp.java
+++ msip-bhu-business-impl/msip_bhu_dataimport/src/main/java/com/bhu/vas/di/op/UserAccessStatisticAnaOp.java
@@ -12,9 +12,6 @@ import java.util.Set;
 import nl.bitwalker.useragentutils.OperatingSystem;
 import nl.bitwalker.useragentutils.UserAgent;
 
-import com.smartwork.msip.cores.helper.ArrayHelper;
-import com.smartwork.msip.cores.helper.StringHelper;
-
 /**
  * 统计操作系统分布图 根据mac地址区别重复数据 进行增加计数
  * 每个操作系统各个型号数量分布
@@ -26,27 +23,38 @@ public class UserAccessStatisticAnaOp {
 		//操作系统
 		//Map<String,Integer> opMap = new HashMap<String,Integer>();
 		//设备型号 系统 型号 数量
-		Map<String,Map<String,Integer>> optypeMap = new HashMap<String,Map<String,Integer>>();
+		Map<String,Map<String,Integer>> opTypeMap = new HashMap<String,Map<String,Integer>>();
 		//出现过的设备mac地址
 		Set<String> macSet = new HashSet<String>();
 		
 		InputStreamReader read = new InputStreamReader(new FileInputStream("/Users/Edmond/Msip.Test/logfile.log"));//考虑到编码格式
         BufferedReader bufferedReader = new BufferedReader(read);
         String lineTxt = null;
+        int total = 0;
         while ((lineTxt = bufferedReader.readLine()) != null) {
         	if (!lineTxt.startsWith("0001") || !lineTxt.endsWith("0000")) continue;
         	DpiInfo dinfo = DpiInfo.fromTextLine(lineTxt);
-        	System.out.println(String.format("%s %s", dinfo.getTmac(),dinfo.getUseragent()));
+        	//if(macSet.contains(dinfo.getTmac())) continue;
+    		//macSet.add(dinfo.getTmac());
+    		//total++;
+        		//System.out.println(String.format("%s %s", handsetMac,result[7]));
+        	//System.out.println(String.format("%s %s", dinfo.getTmac(),dinfo.getUseragent()));
         	if(dinfo.getUseragent() != null){
 	        	UserAgent useragent = new UserAgent(dinfo.getUseragent());
+	        	if(useragent.getOperatingSystem() == OperatingSystem.UNKNOWN) continue;
+	        	
+	        	if(macSet.contains(dinfo.getTmac())) continue;
+	    		macSet.add(dinfo.getTmac());
+	    		total++;
+	        	
 	            System.out.println(useragent.getOperatingSystem());
-	            System.out.println(useragent.getBrowser());
+	            /*System.out.println(useragent.getBrowser());
 	            System.out.println(useragent.getId());
-	            System.out.println(useragent.getBrowserVersion());
+	            System.out.println(useragent.getBrowserVersion());*/
 	            String terminal = T.catchUserAgentTermianl(dinfo.getUseragent());
 	            if(terminal == null){
 	            	if(useragent.getOperatingSystem().toString().startsWith("WIN")){
-	            		terminal = "PC";
+	            		terminal = "未识别PC";
 	            	}
 	            	if(useragent.getOperatingSystem() == OperatingSystem.MAC_OS){
 	            		terminal = "Macintosh";
@@ -64,85 +72,69 @@ public class UserAccessStatisticAnaOp {
 	            		terminal = "iPad";
 	            	}
 	            	
-	            }
-	            System.out.println(terminal);
+	            	if(useragent.getOperatingSystem() == OperatingSystem.ANDROID){
+	            		terminal = "未识别安卓";
+	            	}
+	            }//else terminal = "未识别";
+	            if(terminal.startsWith("zh-cn;")) terminal = terminal.substring(7);
+	            //System.out.println(terminal);
+	            
+	            if(useragent.getOperatingSystem().toString().startsWith("WINDOWS")){
+	            	Map<String, Integer> innermap = opTypeMap.get("WINDOWS");
+	            	if(innermap == null){
+	            		innermap = new HashMap<String,Integer>();
+	            		innermap.put(useragent.getBrowser().toString(), 1);
+	            		opTypeMap.put("WINDOWS", innermap);
+	            	}else{
+	            		Integer hit = innermap.get(useragent.getBrowser().toString());
+	            		if(hit == null){
+	            			innermap.put(useragent.getBrowser().toString(), 1);
+	            		}else{
+	            			hit = hit+1;
+	            			innermap.put(useragent.getBrowser().toString(), hit);
+	            		}
+	            	}
+	        	}
+	            
+	            if(useragent.getOperatingSystem().toString().startsWith("MAC_OS")){
+	            	Map<String, Integer> innermap = opTypeMap.get("MAC_OS");
+	            	if(innermap == null){
+	            		innermap = new HashMap<String,Integer>();
+	            		innermap.put(terminal, 1);
+	            		opTypeMap.put("MAC_OS", innermap);
+	            	}else{
+	            		Integer hit = innermap.get(terminal);
+	            		if(hit == null){
+	            			innermap.put(terminal, 1);
+	            		}else{
+	            			hit = hit+1;
+	            			innermap.put(terminal, hit);
+	            		}
+	            	}
+	        	}
+	            
+	            if(useragent.getOperatingSystem().toString().startsWith("ANDROID")){
+	            	Map<String, Integer> innermap = opTypeMap.get("ANDROID");
+	            	if(innermap == null){
+	            		innermap = new HashMap<String,Integer>();
+	            		innermap.put(terminal, 1);
+	            		opTypeMap.put("ANDROID", innermap);
+	            	}else{
+	            		Integer hit = innermap.get(terminal);
+	            		if(hit == null){
+	            			innermap.put(terminal, 1);
+	            		}else{
+	            			hit = hit+1;
+	            			innermap.put(terminal, hit);
+	            		}
+	            	}
+	        	}
         	}
-        	/*String[] result = parserLine(lineTxt);
-        	if(result == null) continue;
-        	String handsetMac= StringHelper.removeWhiteSpace(result[1]);
-        	//System.out.println(ArrayHelper.toSplitString(result, "|"));
-        	if(!macSet.contains(handsetMac)){
-        		macSet.add(handsetMac);
-        		System.out.println(String.format("%s %s", handsetMac,result[7]));
-        	}*/
         }
         bufferedReader.close();
+        System.out.println(total);
         
-        
+        System.out.println(opTypeMap);
 	}
 	
-	
-	public static String[] parserLine(String lineTxt){
-		if (!lineTxt.startsWith("0001") || !lineTxt.endsWith("0000")) return null;
-		String[] result = new String[8];
-		int currentIndex = 0;
-        String part0001 = lineTxt.substring(currentIndex, currentIndex + 4) +
-                lineTxt.substring(currentIndex + 4, currentIndex + 8) +
-                lineTxt.substring(currentIndex + 8, currentIndex + 8 + Integer.parseInt(lineTxt.substring(currentIndex + 4, currentIndex + 8)));
-        result[0] = part0001;
-        //System.out.println(" --- 任务id : " + part0001);
-        currentIndex = part0001.length();
-
-        String part0002 = lineTxt.substring(currentIndex, currentIndex + 4) +
-                lineTxt.substring(currentIndex + 4, currentIndex + 8) +
-                lineTxt.substring(currentIndex + 8, currentIndex + 8 + Integer.parseInt(lineTxt.substring(currentIndex + 4, currentIndex + 8)) + 1);
-        result[1] = part0002.substring(8);
-        //System.out.println(" --- 客户端MAC : " + part0002);
-        currentIndex +=  part0002.length();
-
-        String part0003 = lineTxt.substring(currentIndex, currentIndex + 4) +
-                lineTxt.substring(currentIndex + 4, currentIndex + 8) +
-                lineTxt.substring(currentIndex + 8, currentIndex + 8 + Integer.parseInt(lineTxt.substring(currentIndex + 4, currentIndex + 8)));
-        //System.out.println(" --- 设备MAC : " + part0003);
-        result[2] = part0003.substring(8);
-        currentIndex +=  part0003.length();
-
-        String part0004 = lineTxt.substring(currentIndex, currentIndex + 4) +
-                lineTxt.substring(currentIndex + 4, currentIndex + 8) +
-                lineTxt.substring(currentIndex + 8, currentIndex + 8 + Integer.parseInt(lineTxt.substring(currentIndex + 4, currentIndex + 8)));
-        //System.out.println(" --- 访问IP : " + part0004);
-        result[3] = part0004;
-        currentIndex +=  part0004.length();
-
-        String part0005 = lineTxt.substring(currentIndex, currentIndex + 4) +
-                lineTxt.substring(currentIndex + 4, currentIndex + 8) +
-                lineTxt.substring(currentIndex + 8, currentIndex + 8 + Integer.parseInt(lineTxt.substring(currentIndex + 4, currentIndex + 8)));
-        //System.out.println(" --- HOST : " + part0005);
-        result[4] = part0005;
-        currentIndex += part0005.length();
-
-        String part0006 = lineTxt.substring(currentIndex, currentIndex + 4) +
-                lineTxt.substring(currentIndex + 4, currentIndex + 8) +
-                lineTxt.substring(currentIndex + 8, currentIndex + 8 + Integer.parseInt(lineTxt.substring(currentIndex + 4, currentIndex + 8)));
-        //System.out.println(" --- URI : " + part0006);
-        result[5] = part0006;
-        currentIndex +=  part0006.length();
-
-        String part0007 = lineTxt.substring(currentIndex, currentIndex + 4) +
-                lineTxt.substring(currentIndex + 4, currentIndex + 8) +
-                lineTxt.substring(currentIndex + 8, currentIndex + 8 + Integer.parseInt(lineTxt.substring(currentIndex + 4, currentIndex + 8)));
-        //System.out.println(" --- ACCEPT : " + part0007);
-        result[6] = part0007.substring(8);
-        currentIndex +=  part0007.length();
-
-        String part0008 = lineTxt.substring(currentIndex, currentIndex + 4) +
-                lineTxt.substring(currentIndex + 4, currentIndex + 8) +
-                lineTxt.substring(currentIndex + 8, currentIndex + 8 + Integer.parseInt(lineTxt.substring(currentIndex + 4, currentIndex + 8)));
-        //System.out.println(" --- USER_AGENT : " + part0008);
-        result[7] = part0008.substring(8);
-        //System.out.println(part0008.substring(8));
-        currentIndex +=  part0008.length();
-        
-        return result;
-	}
 }
