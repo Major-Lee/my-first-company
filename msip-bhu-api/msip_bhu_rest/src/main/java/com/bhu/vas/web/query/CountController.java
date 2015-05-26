d2da5ed6c3883e5fd4049f36da4c404826439c27
Bluesand <bluesand.roann@gmail.com>
2015-05-26 16:44:43 +0800

    fix

--- msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/query/CountController.java
+++ msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/query/CountController.java
@@ -1,12 +1,17 @@
 package com.bhu.vas.web.query;
 
+import com.bhu.vas.api.rpc.user.iservice.IUserDeviceRpcService;
+import com.bhu.vas.api.rpc.vap.iservice.IVapRpcService;
 import com.bhu.vas.msip.cores.web.mvc.spring.BaseController;
+import com.bhu.vas.msip.cores.web.mvc.spring.helper.SpringMVCHelper;
+import com.smartwork.msip.jdo.ResponseSuccess;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,6 +22,9 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/noauth/vap")
 public class CountController extends BaseController {
 
+    @Resource
+    private IVapRpcService vapRpcService;
+
     @ResponseBody()
     @RequestMapping(value="/urlview",method={RequestMethod.POST,RequestMethod.GET})
     public void url404(
@@ -25,6 +33,7 @@ public class CountController extends BaseController {
             @RequestParam(required = true) String key,
             @RequestParam(required = true) String subkey
             ) {
-
+        vapRpcService.urlView(key, subkey);
+        SpringMVCHelper.renderJson(response, ResponseSuccess.SUCCESS);
     }
 }
