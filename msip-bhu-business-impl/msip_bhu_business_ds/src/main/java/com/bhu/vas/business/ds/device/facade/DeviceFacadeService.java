21e0985be22de4f8048358adcd70a27e7d5c6f1e
Edmond Lee <wenhualee@163.com>
2015-05-26 14:00:11 +0800

    added

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/device/facade/DeviceFacadeService.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/device/facade/DeviceFacadeService.java
@@ -491,7 +491,7 @@ public class DeviceFacadeService {
 	 * @throws Exception 
 	 */
 	public String generateDeviceSetting(String mac, String ds_opt, String extparams) throws Exception {
-		if(StringUtils.isEmpty(ds_opt)){
+		if(StringUtils.isEmpty(ds_opt))
 			// || StringUtils.isEmpty(extparams))
 			throw new BusinessI18nCodeException(ResponseErrorCode.TASK_PARAMS_VALIDATE_ILLEGAL);
 		
