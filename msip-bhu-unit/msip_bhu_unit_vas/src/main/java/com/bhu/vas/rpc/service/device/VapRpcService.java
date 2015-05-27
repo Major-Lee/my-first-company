d0abacf4ddfbb859f3e5f9e6fbbfa663fffe10c0
Bluesand <bluesand.roann@gmail.com>
2015-05-27 15:47:09 +0800

    fix

--- msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/service/device/VapRpcService.java
+++ msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/service/device/VapRpcService.java
@@ -22,45 +22,8 @@ public class VapRpcService  implements IVapRpcService{
     @Override
     public void urlView(String key, String field) {
         logger.info(String.format("checkAcc with key[%s] field[%s]", key, field));
-
-        if (vailidateVapMode(key, field)) {
-            vapFacadeService.urlView(key.toLowerCase(), field);
-        }
-
-
+        vapFacadeService.urlView(key.toLowerCase(), field);
     }
 
 
-    /**
-     * 验证url传过来的key
-     * @param key
-     * @param field
-     * @return
-     */
-    private boolean vailidateVapMode(String key, String field) {
-        if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlInjectAdv.getKey())) {
-            if (field.toLowerCase().equals(VapModeDefined.HtmlInjectAdv.STYLE000.getStyle())
-                    || field.toLowerCase().equals(VapModeDefined.HtmlInjectAdv.STYLE001.getStyle())) {
-                return true;
-            }
-        } else if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlInject404.getKey())) {
-            if (field.toLowerCase().equals(VapModeDefined.HtmlInject404.STYLE000.getStyle())
-                    || field.toLowerCase().equals(VapModeDefined.HtmlInject404.STYLE001.getStyle())) {
-                return true;
-            }
-        } else if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlPortal.getKey())) {
-            if (field.toLowerCase().equals(VapModeDefined.HtmlPortal.STYLE000.getStyle())
-                    || field.toLowerCase().equals(VapModeDefined.HtmlPortal.STYLE001.getStyle())) {
-                return true;
-            }
-        } else if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlRedirect.getKey())) {
-            if (field.toLowerCase().equals(VapModeDefined.HtmlRedirect.STYLE000.getStyle())
-                    || field.toLowerCase().equals(VapModeDefined.HtmlRedirect.STYLE001.getStyle())) {
-                return true;
-            }
-        } else {
-            return false;
-        }
-        return false;
-    }
 }
