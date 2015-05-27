20b437d12a09df7ed25b9cdf921861e952dc0a81
Edmond Lee <wenhualee@163.com>
2015-05-27 14:24:22 +0800

    added

--- msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/device/DeviceGroupController.java
+++ msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/device/DeviceGroupController.java
@@ -1,5 +1,7 @@
 package com.bhu.vas.web.device;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,15 +12,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.bhu.vas.api.rpc.devices.iservice.IDeviceURouterRestRpcService;
+import com.bhu.vas.api.rpc.RpcResponseDTO;
+import com.bhu.vas.api.rpc.devices.dto.DeviceGroupDTO;
+import com.bhu.vas.api.rpc.devices.iservice.IDeviceGroupRpcService;
 import com.bhu.vas.msip.cores.web.mvc.spring.BaseController;
+import com.bhu.vas.msip.cores.web.mvc.spring.helper.SpringMVCHelper;
+import com.smartwork.msip.jdo.Response;
+import com.smartwork.msip.jdo.ResponseError;
 
 @Controller
-@RequestMapping("/group")
+@RequestMapping("/devices/group")
 public class DeviceGroupController extends BaseController{
 	
 	@Resource
-	private IDeviceURouterRestRpcService deviceURouterRestRpcService;
+	private IDeviceGroupRpcService deviceGroupRpcService;
+	
+	
+	@ResponseBody()
+	@RequestMapping(value="/birthTree",method={RequestMethod.POST})
+	public void birthTree(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			@RequestParam(required = true) Integer uid,
+			@RequestParam(required = true) Integer pid) {
+		RpcResponseDTO<List<DeviceGroupDTO>> birthTree = deviceGroupRpcService.birthTree(uid, pid);
+		if(birthTree.getErrorCode() == null)
+			SpringMVCHelper.renderJson(response, Response.SUCCESS);
+		else
+			SpringMVCHelper.renderJson(response, ResponseError.embed(birthTree.getErrorCode()));
+	}
 	
 	/**
 	 * 增加及修改群组
@@ -30,8 +52,16 @@ public class DeviceGroupController extends BaseController{
 	public void save(
 			HttpServletRequest request,
 			HttpServletResponse response,
+			@RequestParam(required = true) Integer uid,
 			@RequestParam(required = false) Integer id,
-			@RequestParam(required = true) String name) {
+			@RequestParam(required = true) String name,
+			@RequestParam(required = false) Integer pid
+			) {
+		RpcResponseDTO<DeviceGroupDTO> save = deviceGroupRpcService.save(uid, id, pid, name);
+		if(save.getErrorCode() == null)
+			SpringMVCHelper.renderJson(response, Response.SUCCESS);
+		else
+			SpringMVCHelper.renderJson(response, ResponseError.embed(save.getErrorCode()));
 	}
 	
 	/**
@@ -44,6 +74,7 @@ public class DeviceGroupController extends BaseController{
 	public void detail(
 			HttpServletRequest request,
 			HttpServletResponse response,
+			@RequestParam(required = true) Integer uid,
 			@RequestParam(required = true) Integer id) {
 	}
 	
@@ -58,6 +89,7 @@ public class DeviceGroupController extends BaseController{
 	public void remove(
 			HttpServletRequest request,
 			HttpServletResponse response,
+			@RequestParam(required = true) Integer uid,
 			@RequestParam(required = false) String ids) {
 	}
 	
@@ -73,6 +105,7 @@ public class DeviceGroupController extends BaseController{
 	public void grant(
 			HttpServletRequest request,
 			HttpServletResponse response,
+			@RequestParam(required = true) Integer uid,
 			@RequestParam(required = false) Integer id,
 			@RequestParam(required = false) String wifi_ids) {
 	}
@@ -89,6 +122,7 @@ public class DeviceGroupController extends BaseController{
 	public void ungrant(
 			HttpServletRequest request,
 			HttpServletResponse response,
+			@RequestParam(required = true) Integer uid,
 			@RequestParam(required = false) Integer id,
 			@RequestParam(required = false) String wifi_ids) {
 	}
--- msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/service/device/DeviceGroupRpcService.java
+++ msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/service/device/DeviceGroupRpcService.java
@@ -22,28 +22,33 @@ public class DeviceGroupRpcService implements IDeviceGroupRpcService{
 	@Override
 	public RpcResponseDTO<List<DeviceGroupDTO>> birthTree(Integer uid,
 			Integer pid) {
+		logger.info(String.format("birthTree uid:%s pid:%s",uid,pid));
 		return deviceGroupUnitFacadeRpcService.birthTree(uid, pid);
 	}
 	@Override
 	public RpcResponseDTO<DeviceGroupDTO> save(Integer uid, Integer gid,
 			Integer pid, String name) {
+		logger.info(String.format("save uid:%s gid:%s pid:%s name:%s",uid,gid,pid,name));
 		return deviceGroupUnitFacadeRpcService.save(uid, gid, pid, name);
 	}
 	@Override
 	public RpcResponseDTO<DeviceGroupDTO> detail(Integer uid, Integer gid) {
+		logger.info(String.format("detail uid:%s gid:%s",uid,gid));
 		return deviceGroupUnitFacadeRpcService.detail(uid, gid);
 	}
 	@Override
 	public RpcResponseDTO<Boolean> remove(Integer uid, String gids) {
+		logger.info(String.format("detail uid:%s gids:%s",uid,gids));
 		return deviceGroupUnitFacadeRpcService.remove(uid, gids);
 	}
 	@Override
 	public RpcResponseDTO<Boolean> grant(Integer uid, Integer gid, String wifi_ids) {
+		logger.info(String.format("grant uid:%s gid:%s wifi_ids:%s",uid,gid,wifi_ids));
 		return deviceGroupUnitFacadeRpcService.grant(uid, gid, wifi_ids);
 	}
 	@Override
-	public RpcResponseDTO<Boolean> ungrant(Integer uid, Integer gid,
-			String wifi_ids) {
+	public RpcResponseDTO<Boolean> ungrant(Integer uid, Integer gid,String wifi_ids) {
+		logger.info(String.format("ungrant uid:%s gid:%s wifi_ids:%s",uid,gid,wifi_ids));
 		return deviceGroupUnitFacadeRpcService.ungrant(uid, gid, wifi_ids);
 	}
 
