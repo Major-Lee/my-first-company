c6025c379c0d5e268294c5dcc4940383d1fd8b7e
唐子超 <56755166@qq.com>
2015-06-02 14:17:11 +0800

    1：新增用户设置数据service 2：完善用户push发送逻辑

---
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/dao/UserSettingStateDao.java
@@ -0,0 +1,10 @@
+package com.bhu.vas.business.ds.user.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.bhu.vas.api.rpc.user.model.UserSettingState;
+import com.smartwork.msip.business.abstractmsd.dao.AbstractCoreDao;
+@Repository
+public class UserSettingStateDao extends AbstractCoreDao<Integer,UserSettingState>{
+	
+}
--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/user/dto/UserSettingDTO.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/user/dto/UserSettingDTO.java
@@ -1,20 +1,9 @@
 package com.bhu.vas.api.rpc.user.dto;
 
-@SuppressWarnings("serial")
-public class UserSettingDTO implements java.io.Serializable{
-	private int s_pai;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-	public int getS_pai() {
-		return s_pai;
-	}
-	public void setS_pai(int s_pai) {
-		this.s_pai = s_pai;
-	}
-	public UserSettingDTO() {
-	}
-	public UserSettingDTO(int s_pai) {
-		super();
-		this.s_pai = s_pai;
-	}
-	
+@SuppressWarnings("serial")
+public abstract class UserSettingDTO implements java.io.Serializable{
+	@JsonIgnore
+	public abstract String getSettingKey();
 }
