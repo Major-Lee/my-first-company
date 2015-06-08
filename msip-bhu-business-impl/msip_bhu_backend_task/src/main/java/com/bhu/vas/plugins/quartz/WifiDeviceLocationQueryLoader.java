ac272980aff2b137719e01adeb9267604fb65afd
Edmond Lee <wenhualee@163.com>
2015-06-08 10:32:30 +0800

    added

---
+++ msip-bhu-business-impl/msip_bhu_backend_task/src/main/java/com/bhu/vas/plugins/quartz/WifiDeviceLocationQueryLoader.java
@@ -0,0 +1,47 @@
+package com.bhu.vas.plugins.quartz;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+import com.bhu.vas.api.rpc.daemon.helper.DaemonHelper;
+import com.bhu.vas.api.rpc.daemon.iservice.IDaemonRpcService;
+import com.bhu.vas.api.rpc.devices.model.WifiDevice;
+import com.bhu.vas.business.ds.device.service.WifiDeviceService;
+import com.smartwork.msip.cores.orm.iterator.EntityIterator;
+import com.smartwork.msip.cores.orm.iterator.KeyBasedEntityBatchIterator;
+import com.smartwork.msip.cores.orm.support.criteria.ModelCriteria;
+
+public class WifiDeviceLocationQueryLoader {
+	private static Logger logger = LoggerFactory.getLogger(WifiDeviceLocationQueryLoader.class);
+	
+	@Resource
+	private WifiDeviceService wifiDeviceService;
+	
+	@Resource
+	private IDaemonRpcService daemonRpcService;
+	public void execute() {
+		logger.info("WifiDeviceLocationQueryLoader starting...");
+		ModelCriteria mc = new ModelCriteria();
+		mc.createCriteria().andColumnEqualTo("online", 1).andSimpleCaulse(" 1=1 ");//.andColumnIsNotNull("lat").andColumnIsNotNull("lon");//.andColumnEqualTo("online", 1);
+    	mc.setPageNumber(1);
+    	mc.setPageSize(200);
+    	int count = 0;
+		EntityIterator<String, WifiDevice> it = new KeyBasedEntityBatchIterator<String,WifiDevice>(String.class
+				,WifiDevice.class, wifiDeviceService.getEntityDao(), mc);
+		while(it.hasNext()){
+			List<WifiDevice> entitys = it.next();
+			for(WifiDevice device:entitys){
+				if(StringUtils.isEmpty(device.getLat()) || StringUtils.isEmpty(device.getLon())){
+					DaemonHelper.locationQuery(device.getId(), daemonRpcService);
+					count++;
+				}
+			}
+		}
+		logger.info("WifiDeviceLocationQueryLoader end:"+count);
+	}
+}
