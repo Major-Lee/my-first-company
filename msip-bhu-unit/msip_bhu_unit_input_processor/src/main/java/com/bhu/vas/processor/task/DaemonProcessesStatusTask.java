a32d93b3dc974124b3734bf718e6d258a175557e
Edmond Lee <wenhualee@163.com>
2015-06-15 12:44:19 +0800

    added

--- msip-bhu-unit/msip_bhu_unit_input_processor/src/main/java/com/bhu/vas/processor/task/DaemonProcessesStatusTask.java
+++ msip-bhu-unit/msip_bhu_unit_input_processor/src/main/java/com/bhu/vas/processor/task/DaemonProcessesStatusTask.java
@@ -20,7 +20,7 @@ public class DaemonProcessesStatusTask extends TimerTask{
 		
 		int[] hits = processor.getHits();
 		for(int i=0;i<hits.length;i++){
-			sb.append(String.format("processor index[%s] hit[%s]\n", i));
+			sb.append(String.format("processor index[%s] hit[%s]\n", i,hits[i]));
 		}
 		logger.info(sb.toString());
     	//System.out.println(String.format("DaemonProcessesStatusTask ended! Total[%s] Sended[%s] UnSended[%s] notExistMac[%s]",i,sended,unsended,notExistMac));
