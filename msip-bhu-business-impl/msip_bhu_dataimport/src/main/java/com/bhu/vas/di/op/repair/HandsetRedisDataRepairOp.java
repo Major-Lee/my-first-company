4810888a3f2908e489faf647ba8a24ad75f837e4
Edmond Lee <wenhualee@163.com>
2015-06-27 13:28:06 +0800

    added

--- msip-bhu-business-impl/msip_bhu_dataimport/src/main/java/com/bhu/vas/di/op/repair/HandsetRedisDataRepairOp.java
+++ msip-bhu-business-impl/msip_bhu_dataimport/src/main/java/com/bhu/vas/di/op/repair/HandsetRedisDataRepairOp.java
@@ -1,15 +1,19 @@
 package com.bhu.vas.di.op.repair;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import com.bhu.vas.api.dto.HandsetDeviceDTO;
 import com.bhu.vas.api.rpc.devices.model.HandsetDevice;
-import com.bhu.vas.api.rpc.devices.model.WifiDevice;
 import com.bhu.vas.business.bucache.redis.serviceimpl.devices.WifiDevicePresentCtxService;
 import com.bhu.vas.business.bucache.redis.serviceimpl.devices.WifiDevicePresentService;
+import com.bhu.vas.business.bucache.redis.serviceimpl.handset.HandsetStatisticsService;
+import com.bhu.vas.business.bucache.redis.serviceimpl.handset.HandsetStorageFacadeService;
+import com.bhu.vas.business.ds.builder.BusinessModelBuilder;
 import com.bhu.vas.business.ds.device.service.HandsetDeviceService;
 import com.smartwork.msip.cores.helper.IdHelper;
 import com.smartwork.msip.cores.orm.iterator.EntityIterator;
@@ -35,26 +39,26 @@ public class HandsetRedisDataRepairOp {
 		mc.createCriteria().andSimpleCaulse(" 1=1 ");//.andColumnIsNotNull("lat").andColumnIsNotNull("lon");//.andColumnEqualTo("online", 1);
     	mc.setPageNumber(1);
     	mc.setPageSize(200);
+    	int total = 0;
     	int count = 0;
 		EntityIterator<String, HandsetDevice> it = new KeyBasedEntityBatchIterator<String,HandsetDevice>(String.class
 				,HandsetDevice.class, handsetDeviceService.getEntityDao(), mc);
 		while(it.hasNext()){
 			List<HandsetDevice> entitys = it.next();
-			List<String> ids = IdHelper.getStringIds(entitys);
-			List<String> presents = WifiDevicePresentService.getInstance().getPresents(ids);
-			if(presents !=null && !presents.isEmpty()){
-				int index = 0;
-				for(String ctx:presents){
-					if(!StringUtils.isEmpty(ctx)){
-						String mac = ids.get(index);
-						WifiDevicePresentCtxService.getInstance().addPresent(mac, ctx);
-						System.out.println(String.format("Online device[%s] ctx[%s]", mac,ctx));
-						count++;
-					}
-					index++;
-				}
+			total+= entitys.size();
+			List<HandsetDeviceDTO> dtos = new ArrayList<HandsetDeviceDTO>();
+			for(HandsetDevice hd:entitys){
+				dtos.add(BusinessModelBuilder.fromHandsetDevice(hd));
+			}
+			if(!dtos.isEmpty()){
+				HandsetStorageFacadeService.handsetsComming(dtos);
+				count+=dtos.size();
 			}
 		}
-		System.out.println("数据迁移成功 count:"+count);
+		System.out.println("数据迁移成功 count:"+count +" total:"+total);
+		
+		System.out.println("开始统计redis记录条数");
+		long all = HandsetStorageFacadeService.countAll();
+		System.out.println("开始统计redis记录条数 all:"+all);
 	}
 }
