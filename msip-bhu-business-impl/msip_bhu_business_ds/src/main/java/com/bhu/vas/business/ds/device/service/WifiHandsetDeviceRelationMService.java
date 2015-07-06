4e0c8835e7a638ad9836d8301757a1361f2d5868
Bluesand <bluesand.roann@gmail.com>
2015-07-06 15:39:25 +0800

    fix

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/device/service/WifiHandsetDeviceRelationMService.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/device/service/WifiHandsetDeviceRelationMService.java
@@ -177,11 +177,16 @@ public class WifiHandsetDeviceRelationMService {
                 if (i == 0) {
                     //忽略5分钟之内频繁上下线记录
                     if (!wifiHandsetDeviceItemDetailMDTOList.isEmpty()) {
+
                         if (last_login_at.getTime() - wifiHandsetDeviceItemDetailMDTOList.get(0).getLogout_at()
                                 > 5 * 60 * 1000) {
                             wifiHandsetDeviceItemDetailMDTO.setLogin_at(last_login_at.getTime());
                             wifiHandsetDeviceItemDetailMDTOList.add(0, wifiHandsetDeviceItemDetailMDTO);
                         }
+                    } else {
+                        //如果是第一条数据
+                        wifiHandsetDeviceItemDetailMDTO.setLogin_at(last_login_at.getTime());
+                        wifiHandsetDeviceItemDetailMDTOList.add(0, wifiHandsetDeviceItemDetailMDTO);
                     }
                 }
                 dataMap.put(date, wifiHandsetDeviceItemDetailMDTOList);
