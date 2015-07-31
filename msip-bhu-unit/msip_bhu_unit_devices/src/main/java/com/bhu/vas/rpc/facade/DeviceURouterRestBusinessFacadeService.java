72034dba79fe234dc1a5f9b410c7bd1a954638fd
Bluesand <bluesand.roann@gmail.com>
2015-07-31 14:38:22 +0800

    fix

--- msip-bhu-unit/msip_bhu_unit_devices/src/main/java/com/bhu/vas/rpc/facade/DeviceURouterRestBusinessFacadeService.java
+++ msip-bhu-unit/msip_bhu_unit_devices/src/main/java/com/bhu/vas/rpc/facade/DeviceURouterRestBusinessFacadeService.java
@@ -460,7 +460,8 @@ public class DeviceURouterRestBusinessFacadeService {
 			vto.setOem_swver(device_entity.getOem_swver());
 			vto.setOem_hdver(device_entity.getOem_hdver());
 			vto.setOl(device_entity.isOnline());
-			vto.setUptime(device_entity.getLast_start_at()); //last_start_at -> uptime
+			String last_start_at = device_entity.getLast_start_at();
+			vto.setUptime(last_start_at == null ? String.valueOf(System.currentTimeMillis()) : last_start_at); //last_start_at -> uptime
 			vto.setWan_ip(device_entity.getWan_ip());
 			vto.setIp(device_entity.getIp());
 			//vto.setMode(DeviceHelper.getDeviceMode(setting_dto));
