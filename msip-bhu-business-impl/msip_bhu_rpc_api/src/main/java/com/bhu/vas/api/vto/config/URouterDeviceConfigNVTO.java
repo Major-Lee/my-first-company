afa30f390e706ef0ba739c623eb337e44548f24a
Bluesand <bluesand.roann@gmail.com>
2015-08-31 20:52:28 +0800

    fix

---
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/config/URouterDeviceConfigNVTO.java
@@ -0,0 +1,29 @@
+package com.bhu.vas.api.vto.config;
+
+import java.io.Serializable;
+
+/** 终端名称
+ * Created by bluesand on 8/31/15.
+ */
+public class URouterDeviceConfigNVTO  implements Serializable {
+
+    private String mac;
+
+    private String n;
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+}
--- msip-bhu-unit/msip_bhu_unit_devices/src/main/java/com/bhu/vas/rpc/facade/DeviceURouterRestBusinessFacadeService.java
+++ msip-bhu-unit/msip_bhu_unit_devices/src/main/java/com/bhu/vas/rpc/facade/DeviceURouterRestBusinessFacadeService.java
@@ -5,6 +5,7 @@ import java.util.*;
 import javax.annotation.Resource;
 
 import com.bhu.vas.api.mdto.WifiHandsetDeviceItemLogMDTO;
+import com.bhu.vas.api.vto.config.URouterDeviceConfigNVTO;
 import com.smartwork.msip.cores.helper.*;
 import org.apache.xpath.operations.Equals;
 import org.slf4j.Logger;
@@ -995,7 +996,18 @@ public class DeviceURouterRestBusinessFacadeService {
 			//黑名单列表
 			WifiDeviceSettingAclDTO acl_dto = DeviceHelper.matchDefaultAcl(setting_dto);
 			if(acl_dto != null){
-				vto.setBlock_macs(acl_dto.getMacs());
+
+				List<HandsetDeviceDTO> handsets = HandsetStorageFacadeService.handsets(acl_dto.getMacs());
+
+				List<URouterDeviceConfigNVTO> blocks = new ArrayList<URouterDeviceConfigNVTO>();
+
+				for (HandsetDeviceDTO dto: handsets) {
+					URouterDeviceConfigNVTO nvto = new URouterDeviceConfigNVTO();
+					nvto.setMac(dto.getMac());
+					nvto.setN(dto.getDhcp_name());
+				}
+				
+				vto.setBlock_macs(blocks);
 			}
 			//终端别名
 			List<WifiDeviceSettingMMDTO> mm_dtos = setting_dto.getMms();
