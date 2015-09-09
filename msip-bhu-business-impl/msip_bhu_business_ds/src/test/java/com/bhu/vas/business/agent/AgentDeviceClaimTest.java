817c07b9a6aae5944a62e7676f77f5f78e2b3edb
Bluesand <bluesand.roann@gmail.com>
2015-09-09 09:34:28 +0800

    remove poi test

--- msip-bhu-business-impl/msip_bhu_business_ds/src/test/java/com/bhu/vas/business/agent/AgentDeviceClaimTest.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/test/java/com/bhu/vas/business/agent/AgentDeviceClaimTest.java
@@ -46,43 +46,43 @@ public class AgentDeviceClaimTest extends BaseTest {
     }
 
 
-    @Test
-    public void excel() throws Exception{
-        InputStream is = new FileInputStream("/Users/bluesand/Desktop/workbook.xls");
-        HSSFWorkbook hssfWorkbook = new HSSFWorkbook(is);
-        AgentDeviceClaim agentDeviceClaim = null;
-        List<AgentDeviceClaim> list = new ArrayList<AgentDeviceClaim>();
-
-        for (int numSheet = 0; numSheet <hssfWorkbook.getNumberOfSheets(); numSheet++) {
-            System.out.println(String.format("numSheet[%s]",numSheet));
-            HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(numSheet);
-            if (hssfSheet == null) {
-                continue;
-            }
-            for (int rowNum = 1; rowNum <= hssfSheet.getLastRowNum(); rowNum++) {
-                HSSFRow hssfRow = hssfSheet.getRow(rowNum);
-                if (hssfRow == null) {
-                    continue;
-                }
-
-                agentDeviceClaim = new AgentDeviceClaim();
-
-                HSSFCell uid = hssfRow.getCell(0);
-
-                if (uid == null) {
-                    continue;
-                }
-                agentDeviceClaim.setUid((int)uid.getNumericCellValue());
-
-                HSSFCell sn = hssfRow.getCell(1);
-
-                agentDeviceClaim.setId(sn.getStringCellValue());
-
-                Date date = new Date();
-                agentDeviceClaim.setSold_at(date);
-                agentDeviceClaimService.insert(agentDeviceClaim);
-            }
-        }
-    }
+//    @Test
+//    public void excel() throws Exception{
+//        InputStream is = new FileInputStream("/Users/bluesand/Desktop/workbook.xls");
+//        HSSFWorkbook hssfWorkbook = new HSSFWorkbook(is);
+//        AgentDeviceClaim agentDeviceClaim = null;
+//        List<AgentDeviceClaim> list = new ArrayList<AgentDeviceClaim>();
+//
+//        for (int numSheet = 0; numSheet <hssfWorkbook.getNumberOfSheets(); numSheet++) {
+//            System.out.println(String.format("numSheet[%s]",numSheet));
+//            HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(numSheet);
+//            if (hssfSheet == null) {
+//                continue;
+//            }
+//            for (int rowNum = 1; rowNum <= hssfSheet.getLastRowNum(); rowNum++) {
+//                HSSFRow hssfRow = hssfSheet.getRow(rowNum);
+//                if (hssfRow == null) {
+//                    continue;
+//                }
+//
+//                agentDeviceClaim = new AgentDeviceClaim();
+//
+//                HSSFCell uid = hssfRow.getCell(0);
+//
+//                if (uid == null) {
+//                    continue;
+//                }
+//                agentDeviceClaim.setUid((int)uid.getNumericCellValue());
+//
+//                HSSFCell sn = hssfRow.getCell(1);
+//
+//                agentDeviceClaim.setId(sn.getStringCellValue());
+//
+//                Date date = new Date();
+//                agentDeviceClaim.setSold_at(date);
+//                agentDeviceClaimService.insert(agentDeviceClaim);
+//            }
+//        }
+//    }
 
 }
