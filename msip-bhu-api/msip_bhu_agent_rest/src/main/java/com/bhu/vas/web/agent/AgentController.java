05ec7f15b5240d32d75b3b0d6f9f860782d1fd09
Edmond Lee <wenhualee@163.com>
2015-09-10 15:13:23 +0800

    added

--- msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/agent/AgentController.java
+++ msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/agent/AgentController.java
@@ -1,11 +1,12 @@
 package com.bhu.vas.web.agent;
 
-import com.bhu.vas.api.rpc.agent.iservice.IAgentRpcService;
-import com.bhu.vas.msip.cores.web.mvc.spring.helper.SpringMVCHelper;
-import com.smartwork.msip.cores.helper.FileHelper;
-import com.smartwork.msip.cores.helper.StringHelper;
-import com.smartwork.msip.jdo.ResponseError;
-import com.smartwork.msip.jdo.ResponseSuccess;
+import java.io.File;
+import java.util.Date;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.util.Date;
+import com.bhu.vas.api.rpc.agent.iservice.IAgentRpcService;
+import com.bhu.vas.msip.cores.web.mvc.spring.helper.SpringMVCHelper;
+import com.smartwork.msip.jdo.ResponseSuccess;
 
 /**
  * Created by bluesand on 9/9/15.
