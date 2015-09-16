7c6d82967e8da1072e1be36833749c79548a7e76
Edmond Lee <wenhualee@163.com>
2015-09-16 18:09:20 +0800

    当日期为空时的处理

--- msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/agent/AgentController.java
+++ msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/agent/AgentController.java
@@ -19,6 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,6 +32,7 @@ import com.bhu.vas.api.rpc.agent.vto.DailyRevenueRecordVTO;
 import com.bhu.vas.api.rpc.agent.vto.StatisticsVTO;
 import com.bhu.vas.api.vto.agent.AgentDeviceClaimVTO;
 import com.bhu.vas.msip.cores.web.mvc.spring.helper.SpringMVCHelper;
+import com.smartwork.msip.cores.helper.DateTimeHelper;
 import com.smartwork.msip.cores.helper.JsonHelper;
 import com.smartwork.msip.cores.orm.support.page.TailPage;
 import com.smartwork.msip.jdo.ResponseError;
@@ -52,6 +54,9 @@ public class AgentController {
                            @RequestParam(required = true) Integer uid,
                            @RequestParam(required = false) String date){
     	try{
+    		if(StringUtils.isEmpty(date)){
+    			date = DateTimeHelper.formatDate(DateTimeHelper.getDateDaysAgo(1),DateTimeHelper.FormatPattern5);
+    		}
 			RpcResponseDTO<StatisticsVTO> rpcResult = agentRpcService.statistics(uid, date);
 			if(!rpcResult.hasError())
 				SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
@@ -72,6 +77,9 @@ public class AgentController {
             @RequestParam(required = false, defaultValue = "20", value = "ps") int pageSize
     		){
     	try{
+    		if(StringUtils.isEmpty(date)){
+    			date = DateTimeHelper.formatDate(DateTimeHelper.getDateDaysAgo(1),DateTimeHelper.FormatPattern5);
+    		}
 			RpcResponseDTO<TailPage<DailyRevenueRecordVTO>> rpcResult = agentRpcService.pageHistoryRecords(uid, date, pageNo, pageSize);
 			if(!rpcResult.hasError())
 				SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
