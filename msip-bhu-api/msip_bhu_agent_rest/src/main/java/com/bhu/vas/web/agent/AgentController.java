a36478131e3aba8478e70ad740511c3dae6d0cc1
Bluesand <bluesand.roann@gmail.com>
2015-09-16 13:38:16 +0800

    fix

--- msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/agent/AgentController.java
+++ msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/agent/AgentController.java
@@ -174,7 +174,7 @@ public class AgentController {
             AgentOutputDTO dto = JsonHelper.getDTO(content, AgentOutputDTO.class);
             String path = dto.getPath();
             if (path != null) {
-                //headers.setContentDispositionFormData("attachment", "resutl.xls");
+                headers.setContentDispositionFormData("attachment", "resutl.xls");
                 File file = new File(path);
                 return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.CREATED);
             }
