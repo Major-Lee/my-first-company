66d18e0d31c8b214bfd538f2b5f5ef8ff9884016
Bluesand <bluesand.roann@gmail.com>
2015-09-29 15:14:25 +0800

    fix

--- msip-bhu-business-impl/msip_bhu_business_ds/src/test/java/com/bhu/vas/business/agent/AgentDeviceClaimTest.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/test/java/com/bhu/vas/business/agent/AgentDeviceClaimTest.java
@@ -78,11 +78,11 @@ public class AgentDeviceClaimTest extends BaseTest {
                 vto = new AgentDeviceImportLogVTO();
                 vto.setAid(log.getAid());
                 vto.setCount(log.getCount());
-                vto.setCreated_at(log.getCreated_at().getTime());
+                vto.setCreated_at(log.getCreated_at());
 
                 User agent = userService.getById(log.getAid());
                 if (agent != null) {
-                    vto.setName(agent.getNick() == null ? "" : agent.getNick());
+                    vto.setNick(agent.getNick() == null ? "" : agent.getNick());
                 }
                 vtos.add(vto);
             }
