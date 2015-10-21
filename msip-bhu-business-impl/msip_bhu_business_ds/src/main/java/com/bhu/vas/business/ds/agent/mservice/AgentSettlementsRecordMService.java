469c35633ebb28be32e084ea07e9aea7cfafae46
Edmond Lee <wenhualee@163.com>
2015-10-21 13:42:16 +0800

    added

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/agent/mservice/AgentSettlementsRecordMService.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/agent/mservice/AgentSettlementsRecordMService.java
@@ -182,8 +182,8 @@ public class AgentSettlementsRecordMService {
 	 */
 	public SettlementStatisticsVTO statistics(int agent){
 		SettlementStatisticsVTO result = new SettlementStatisticsVTO();
-		Criteria unsettled_criteria = Criteria.where("status").in(new int[]{AgentSettlementsRecordMDTO.Settlement_Bill_Created,AgentSettlementsRecordMDTO.Settlement_Bill_Parted});
-		Criteria settled_criteria = Criteria.where("status").in(new int[]{AgentSettlementsRecordMDTO.Settlement_Bill_Done});
+		Criteria unsettled_criteria = Criteria.where("status").in(new Object[]{AgentSettlementsRecordMDTO.Settlement_Bill_Created,AgentSettlementsRecordMDTO.Settlement_Bill_Parted});
+		Criteria settled_criteria = Criteria.where("status").in(new Object[]{AgentSettlementsRecordMDTO.Settlement_Bill_Done});
 		if(agent>0){
 			unsettled_criteria.and("agent").is(agent);
 			settled_criteria.and("agent").is(agent);
