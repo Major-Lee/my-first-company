79d28ecf3eff36853eb9e37f205e0d4d358223c4
Bluesand <bluesand.roann@gmail.com>
2015-10-21 16:54:31 +0800

    fix

--- msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/financial/FinancialController.java
+++ msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/financial/FinancialController.java
@@ -1,6 +1,7 @@
 package com.bhu.vas.web.financial;
 
 import com.bhu.vas.api.rpc.agent.iservice.IAgentRpcService;
+import com.bhu.vas.api.vto.agent.AgentFinancialUploadVTO;
 import com.bhu.vas.msip.cores.web.mvc.spring.helper.SpringMVCHelper;
 import com.smartwork.msip.jdo.ResponseError;
 import com.smartwork.msip.jdo.ResponseSuccess;
@@ -13,6 +14,8 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.Date;
 
 /**
  * Created by bluesand on 10/21/15.
@@ -20,6 +23,15 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("/financial")
 public class FinancialController {
+
+
+    private final static String URL_PREFIX = "http://192.168.66.7/img/" ;
+
+    private final static int UPLOAD_TYPE_INVOICE = 1;
+    private final static int UPLOAD_TYPE_RECEIPT = 2;
+
+    private final static String INVOICE = "invoice";
+    private final static String RECEIPT = "receipt";
 
     @Resource
     private IAgentRpcService agentRpcService;
@@ -47,23 +59,61 @@ public class FinancialController {
     }
 
 
-//    @ResponseBody()
-//    @RequestMapping(value="/upload")
-//    public void uploadFinanical(
-//            HttpServletRequest request,
-//            HttpServletResponse response,
-//            @RequestParam(required = true) Integer uid,
-//            @RequestParam(required = true) Integer type,
-//            @RequestParam("file") CommonsMultipartFile file
-//
-//    ) {
-//        try {
-//
-//            SpringMVCHelper.renderJson(response, ResponseSuccess.embed(ret));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            SpringMVCHelper.renderJson(response, ResponseError.BUSINESS_ERROR);
-//        }
-//
-//    }
+    @ResponseBody()
+    @RequestMapping(value="/upload")
+    public void uploadFinanical(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestParam(required = true) Integer uid,
+            @RequestParam(required = true) Integer type,
+            @RequestParam(required = true) Integer aid,
+            @RequestParam("file") CommonsMultipartFile file
+
+    ) {
+        try {
+
+            String dirPath = null;
+            String uploadType = null;
+            if (type == UPLOAD_TYPE_INVOICE) {
+                dirPath = IAgentRpcService.PATH_INVOICE_PREFIX + File.separator + aid;
+                uploadType = INVOICE;
+            }
+
+            if (type == UPLOAD_TYPE_RECEIPT) {
+                dirPath = IAgentRpcService.PATH_RECEIPT_PREFIX + File.separator + aid;
+                uploadType = RECEIPT;
+            }
+
+            //todo(bluesand): 方法提取
+            File dirFile = new File(dirPath);
+            if (dirFile.exists()) {
+                if (!dirFile.isDirectory()) {
+                    dirFile.delete();
+                    dirFile = new File(dirPath);
+                    dirFile.mkdirs();
+                }
+            } else {
+                dirFile.mkdirs();
+            }
+
+            int index = file.getOriginalFilename().lastIndexOf(".");
+            String  ext = file.getOriginalFilename().substring(index);
+            Date date = new Date();
+            String filePath = dirPath + File.separator + date.getTime() + ext;
+            File newFile = new File(filePath);
+            file.transferTo(newFile);
+
+
+            AgentFinancialUploadVTO vto = new AgentFinancialUploadVTO();
+            vto.setUid(uid);
+            vto.setAid(aid);
+            vto.setUrl(URL_PREFIX + uploadType + File.separator + date.getTime() + ext);
+
+            SpringMVCHelper.renderJson(response, ResponseSuccess.embed(vto));
+        } catch (Exception e) {
+            e.printStackTrace();
+            SpringMVCHelper.renderJson(response, ResponseError.BUSINESS_ERROR);
+        }
+
+    }
 }
---
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/agent/AgentFinancialUploadVTO.java
@@ -0,0 +1,35 @@
+package com.bhu.vas.api.vto.agent;
+
+import java.io.Serializable;
+
+/**
+ * Created by bluesand on 10/21/15.
+ */
+public class AgentFinancialUploadVTO implements Serializable {
+    private int uid;
+    private int aid;
+    private String url;
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
