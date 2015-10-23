e67a0f6d1ec595026422d2af956ec61bb8ced53b
Edmond Lee <wenhualee@163.com>
2015-10-23 16:47:13 +0800

    added

--- msip-bhu-business-impl/msip_bhu_dataimport/src/main/java/com/bhu/vas/di/op/charging/DailyChargingDataParserOper.java
+++ msip-bhu-business-impl/msip_bhu_dataimport/src/main/java/com/bhu/vas/di/op/charging/DailyChargingDataParserOper.java
@@ -9,11 +9,9 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 import com.bhu.vas.di.business.datainit.charging.ParserLog;
 import com.bhu.vas.di.business.datainit.charging.Step00ReadAndMergeLogService;
 import com.bhu.vas.di.business.datainit.charging.Step00ReadSimulateLogService;
-import com.bhu.vas.di.business.datainit.charging.Step01Result2FileService;
 import com.bhu.vas.di.business.datainit.charging.Step02DeviceWholeDayRecordService;
 import com.bhu.vas.di.business.datainit.charging.Step04DeviceWholeMonthRecordService;
 import com.bhu.vas.di.business.datainit.charging.Step05AgentWholeDayRecordService;
-import com.bhu.vas.di.business.datainit.charging.Step10AgentDeviceSimulateDateGenService;
 import com.smartwork.msip.cores.helper.DateTimeHelper;
 import com.smartwork.msip.cores.orm.iterator.IteratorNotify;
 
@@ -26,20 +24,36 @@ import com.smartwork.msip.cores.orm.iterator.IteratorNotify;
 public class DailyChargingDataParserOper {
 
 	public static void main(String[] argv) throws UnsupportedEncodingException, IOException{
+		if(argv == null || argv.length < 3){
+			System.out.println("参数不全 $dates(yyyy-MM-dd) $simulatelogpath $charginglogpath ");
+			return;
+		}
+		String[] dates = argv[0].split(",");
+		String chargingsimulogs = argv[1];//BHUData/bulogs/copylogs/%s/chargingsimulogs/
+		String charginglogs = argv[2];//BHUData/bulogs/copylogs/%s/charginglogs/
+		
+		System.out.println("----------Params------------");
+		for(String date:dates){
+			System.out.println("日期:"+date);
+		}
+		System.out.println("模拟登录日志:"+chargingsimulogs);
+		System.out.println("登录登出日志:"+charginglogs);
+		System.out.println("----------Params End------------");
+		
 		//String date = "2015-09-10";
 		ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath*:com/bhu/vas/di/business/dataimport/dataImportCtx.xml");
 		
 		//Step00ParserLogService step00ParserLogService = (Step00ParserLogService)ctx.getBean("step00ParserLogService");
 		Step00ReadSimulateLogService step00ReadSimulateLogService = (Step00ReadSimulateLogService)ctx.getBean("step00ReadSimulateLogService");
 		Step00ReadAndMergeLogService step00ReadAndMergeLogService = (Step00ReadAndMergeLogService)ctx.getBean("step00ReadAndMergeLogService");
-		Step01Result2FileService step01Result2FileService = (Step01Result2FileService)ctx.getBean("step01Result2FileService");
+		//Step01Result2FileService step01Result2FileService = (Step01Result2FileService)ctx.getBean("step01Result2FileService");
 		Step02DeviceWholeDayRecordService step02DeviceWholeDayRecordService = (Step02DeviceWholeDayRecordService)ctx.getBean("step02DeviceWholeDayRecordService");
 		
 		Step04DeviceWholeMonthRecordService step04DeviceWholeMonthRecordService = (Step04DeviceWholeMonthRecordService)ctx.getBean("step04DeviceWholeMonthRecordService");
 		Step05AgentWholeDayRecordService step05AgentWholeDayRecordService = (Step05AgentWholeDayRecordService)ctx.getBean("step05AgentWholeDayRecordService");
-		Step10AgentDeviceSimulateDateGenService step10AgentDeviceSimulateDateGenService = (Step10AgentDeviceSimulateDateGenService)ctx.getBean("step10AgentDeviceSimulateDateGenService");
+		//Step10AgentDeviceSimulateDateGenService step10AgentDeviceSimulateDateGenService = (Step10AgentDeviceSimulateDateGenService)ctx.getBean("step10AgentDeviceSimulateDateGenService");
 		long ts1 = System.currentTimeMillis();
-		String[] dates = new String[]{"2015-10-17","2015-10-18","2015-10-19","2015-10-20"};
+		//String[] dates = new String[]{"2015-10-17","2015-10-18","2015-10-19","2015-10-20"};
 		//String[] dates = new String[]{"2015-09-10","2015-09-11","2015-09-12","2015-09-13"};
 		//String[] dates = new String[]{"2015-09-14","2015-09-15","2015-09-16","2015-09-17"};
 		//String[] dates = new String[]{"2015-09-18","2015-09-19","2015-09-20","2015-09-21"};
@@ -58,7 +72,7 @@ public class DailyChargingDataParserOper {
 						e.printStackTrace(System.out);
 					}
 				}
-			}, "/BHUData/bulogs/chargingsimulogs/");
+			}, String.format(chargingsimulogs, date));//"/BHUData/bulogs/chargingsimulogs/");
 			//DailyChargingDataParserOp op = new DailyChargingDataParserOp();
 			//step00ParserLogService.reset();
 			step00ReadAndMergeLogService.parser(date,new IteratorNotify<String>(){
@@ -79,7 +93,7 @@ public class DailyChargingDataParserOper {
 						}
 					}*/
 				}
-			},"/BHUData/bulogs/charginglogs-a/");
+			},String.format(charginglogs, date));//"/BHUData/bulogs/charginglogs-a/");
 			parser.processEnd(parser.getDevice_records());
 			parser.filter();
 			System.out.println("Device_records size:"+parser.getDevice_records().size());
