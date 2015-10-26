24d58ed064c6cc8dc733df3f8d2b83b9740ba5ff
Bluesand <bluesand.roann@gmail.com>
2015-10-26 16:25:23 +0800

    访客网络

---
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/guest/URouterGuestListVTO.java
@@ -0,0 +1,60 @@
+package com.bhu.vas.api.vto.guest;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by bluesand on 10/26/15.
+ */
+public class URouterGuestListVTO implements Serializable {
+
+    private String mac;
+
+    private String n;
+
+    private int ohd_count;
+
+    private int limit_rate;
+
+    private List<URouterGuestDetailVTO> items;
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+
+    public int getOhd_count() {
+        return ohd_count;
+    }
+
+    public void setOhd_count(int ohd_count) {
+        this.ohd_count = ohd_count;
+    }
+
+    public int getLimit_rate() {
+        return limit_rate;
+    }
+
+    public void setLimit_rate(int limit_rate) {
+        this.limit_rate = limit_rate;
+    }
+
+    public List<URouterGuestDetailVTO> getItems() {
+        return items;
+    }
+
+    public void setItems(List<URouterGuestDetailVTO> items) {
+        this.items = items;
+    }
+}
