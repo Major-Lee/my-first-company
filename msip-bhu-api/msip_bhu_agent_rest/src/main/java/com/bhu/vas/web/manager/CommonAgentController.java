11d7faa91b641aa756f51f6bb86383debaab1309
Bluesand <bluesand.roann@gmail.com>
2015-11-02 14:59:32 +0800

    refactor agent

--- msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/manager/CommonAgentController.java
+++ msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/manager/CommonAgentController.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/common")
-public class CommonController {
+public class CommonAgentController {
 
     @Resource
     private IAgentRpcService agentRpcService;
