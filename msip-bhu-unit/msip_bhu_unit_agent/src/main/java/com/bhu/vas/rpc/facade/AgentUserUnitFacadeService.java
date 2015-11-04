a5494bebef82d55f50e31f162ae3684fa7aac54c
Edmond Lee <wenhualee@163.com>
2015-11-04 16:47:05 +0800

    added

--- msip-bhu-unit/msip_bhu_unit_agent/src/main/java/com/bhu/vas/rpc/facade/AgentUserUnitFacadeService.java
+++ msip-bhu-unit/msip_bhu_unit_agent/src/main/java/com/bhu/vas/rpc/facade/AgentUserUnitFacadeService.java
@@ -240,7 +240,7 @@ public class AgentUserUnitFacadeService {
 			if(keywords!=null && StringUtils.isNotEmpty(keywords.trim())){
 				cri.andColumnLike("org", "%"+keywords+"%");
 			}
-			mc.setOrderByClause("id");
+			mc.setOrderByClause(" id ");
 			mc.setPageNumber(pageno);
 			mc.setPageSize(pagesize);
 			TailPage<User> tailusers = this.userService.findModelTailPageByModelCriteria(mc);
