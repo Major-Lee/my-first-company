23028cbcb4c778c3f4026839a8b8908d88282e6f
Bluesand <bluesand.roann@gmail.com>
2015-11-05 16:14:02 +0800

    fix

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/agent/dto/AgentOutputDTO.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/agent/dto/AgentOutputDTO.java
@@ -14,6 +14,10 @@ public class AgentOutputDTO implements Serializable{
 
     private String name;
 
+    private int success_count;
+
+    private int fail_count;
+
     public int getAid() {
         return aid;
     }
@@ -36,5 +40,21 @@ public class AgentOutputDTO implements Serializable{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getSuccess_count() {
+        return success_count;
+    }
+
+    public void setSuccess_count(int success_count) {
+        this.success_count = success_count;
+    }
+
+    public int getFail_count() {
+        return fail_count;
+    }
+
+    public void setFail_count(int fail_count) {
+        this.fail_count = fail_count;
     }
 }
