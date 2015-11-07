baac7feda44ee4af1000b8cab8d4ecb4c149a428
Edmond Lee <wenhualee@163.com>
2015-11-07 12:01:37 +0800

    added

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/device/facade/WifiDeviceGrayFacadeService.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/device/facade/WifiDeviceGrayFacadeService.java
@@ -301,6 +301,9 @@ public class WifiDeviceGrayFacadeService {
 			if(dvparser.wasDutURouter()){
 				dut = VapEnumType.DeviceUnitType.uRouterTU.getIndex();
 				gl = VapEnumType.GrayLevel.Other.getIndex();
+			}else{
+				System.out.println("A deviceUpgradeAutoAction unsupported!");
+				return null;
 			}
 		}else{
 			dut = deviceUnitGrayPk.getDut();
