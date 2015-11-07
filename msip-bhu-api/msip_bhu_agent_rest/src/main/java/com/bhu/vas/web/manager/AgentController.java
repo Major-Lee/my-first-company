7291af16c395045689ff470fca9ce22149e49e43
Bluesand <bluesand.roann@gmail.com>
2015-11-07 16:45:12 +0800

    fix

--- msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/manager/AgentController.java
+++ msip-bhu-api/msip_bhu_agent_rest/src/main/java/com/bhu/vas/web/manager/AgentController.java
@@ -229,7 +229,7 @@ public class AgentController {
      * @throws IOException
      */
     @ResponseBody()
-    @RequestMapping(value="/download")
+    @RequestMapping(value="/download", method={RequestMethod.GET})
     public ResponseEntity<byte[]> downloadClaimAgentDevice (
             HttpServletRequest request,
             HttpServletResponse response,
