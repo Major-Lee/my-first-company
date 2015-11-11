aa2bcbb2926b572e494964c5699e602efbff2e8c
Bluesand <bluesand.roann@gmail.com>
2015-11-11 15:02:48 +0800

    fix

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/service/UserService.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/service/UserService.java
@@ -73,7 +73,13 @@ public class UserService extends AbstractCoreService<Integer,User, UserDao>{//En
 		mc.createCriteria().andSimpleCaulse("1=1").andColumnEqualTo("org", org);
 		List<Integer> ids = this.findIdsByCommonCriteria(mc);
 		return !(ids == null || ids.isEmpty());
+	}
 
+	public boolean isExistsOrg(int uid, String org) {
+		ModelCriteria mc = new ModelCriteria();
+		mc.createCriteria().andSimpleCaulse("1=1").andColumnNotEqualTo("id", uid).andColumnEqualTo("org", org);
+		List<Integer> ids = this.findIdsByCommonCriteria(mc);
+		return !(ids == null || ids.isEmpty());
 	}
 
 
