0587065a094e0287f7fcb7b6db371e0b0701267b
唐子超 <tangzichao@tangzichaodeMacBook-Pro.local>
2015-11-26 16:06:59 +0800

    用户保存搜索条件的存储信息中增加desc字段

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/SearchConditionVTO.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/SearchConditionVTO.java
@@ -1,6 +1,8 @@
 package com.bhu.vas.api.vto;
 
 import java.io.Serializable;
+
+import com.bhu.vas.api.dto.redis.StoreSearchConditionDTO;
 /**
  * 搜索条件vto
  * @author tangzichao
@@ -11,7 +13,7 @@ public class SearchConditionVTO implements Serializable{
 	//搜索条件保存的时间戳
 	private long ts;
 	//搜索条件数据
-	private String message;
+	private StoreSearchConditionDTO sdto;
 	
 	public long getTs() {
 		return ts;
@@ -19,10 +21,10 @@ public class SearchConditionVTO implements Serializable{
 	public void setTs(long ts) {
 		this.ts = ts;
 	}
-	public String getMessage() {
-		return message;
+	public StoreSearchConditionDTO getSdto() {
+		return sdto;
 	}
-	public void setMessage(String message) {
-		this.message = message;
+	public void setSdto(StoreSearchConditionDTO sdto) {
+		this.sdto = sdto;
 	}
 }
