a67909086c198b4fd73c8ff2e5a336a000f33142
Bluesand <bluesand.roann@gmail.com>
2016-01-05 20:29:36 +0800

    注释掉注入统计的验证

--- msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/facade/VapFacadeService.java
+++ msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/facade/VapFacadeService.java
@@ -48,30 +48,30 @@ public class VapFacadeService {
      * @return
      */
     private boolean vailidateVapMode(String key, String field) {
-        if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlInjectAdv.getKey())) {
-            if (field.toLowerCase().equals(VapModeDefined.HtmlInjectAdv.STYLE000.getStyle())
-                    || field.toLowerCase().equals(VapModeDefined.HtmlInjectAdv.STYLE001.getStyle())) {
-                return true;
-            }
-        } else if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlInject404.getKey())) {
-            if (field.toLowerCase().equals(VapModeDefined.HtmlInject404.STYLE000.getStyle())
-                    || field.toLowerCase().equals(VapModeDefined.HtmlInject404.STYLE001.getStyle())) {
-                return true;
-            }
-        } else if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlPortal.getKey())) {
-            if (field.toLowerCase().equals(VapModeDefined.HtmlPortal.STYLE000.getStyle())
-                    || field.toLowerCase().equals(VapModeDefined.HtmlPortal.STYLE001.getStyle())) {
-                return true;
-            }
-        } else if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlRedirect.getKey())) {
-            if (field.toLowerCase().equals(VapModeDefined.HtmlRedirect.STYLE000.getStyle())
-                    || field.toLowerCase().equals(VapModeDefined.HtmlRedirect.STYLE001.getStyle())) {
-                return true;
-            }
-        } else {
-            return false;
-        }
-        return false;
+//        if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlInjectAdv.getKey())) {
+//            if (field.toLowerCase().equals(VapModeDefined.HtmlInjectAdv.STYLE000.getStyle())
+//                    || field.toLowerCase().equals(VapModeDefined.HtmlInjectAdv.STYLE001.getStyle())) {
+//                return true;
+//            }
+//        } else if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlInject404.getKey())) {
+//            if (field.toLowerCase().equals(VapModeDefined.HtmlInject404.STYLE000.getStyle())
+//                    || field.toLowerCase().equals(VapModeDefined.HtmlInject404.STYLE001.getStyle())) {
+//                return true;
+//            }
+//        } else if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlPortal.getKey())) {
+//            if (field.toLowerCase().equals(VapModeDefined.HtmlPortal.STYLE000.getStyle())
+//                    || field.toLowerCase().equals(VapModeDefined.HtmlPortal.STYLE001.getStyle())) {
+//                return true;
+//            }
+//        } else if (key.toLowerCase().equals(VapModeDefined.VapMode.HtmlRedirect.getKey())) {
+//            if (field.toLowerCase().equals(VapModeDefined.HtmlRedirect.STYLE000.getStyle())
+//                    || field.toLowerCase().equals(VapModeDefined.HtmlRedirect.STYLE001.getStyle())) {
+//                return true;
+//            }
+//        } else {
+//            return false;
+//        }
+//        return false;
     }
 
     /**
