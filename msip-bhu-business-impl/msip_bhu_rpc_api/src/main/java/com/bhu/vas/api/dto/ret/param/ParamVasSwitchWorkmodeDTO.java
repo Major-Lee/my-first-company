b69d7661d4462ede5ab71bc1e70925dfa66baa53
Edmond Lee <wenhualee@163.com>
2016-01-11 14:41:33 +0800

    修改模式切换

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/dto/ret/param/ParamVasSwitchWorkmodeDTO.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/dto/ret/param/ParamVasSwitchWorkmodeDTO.java
@@ -15,17 +15,14 @@ public class ParamVasSwitchWorkmodeDTO{
 	public int getWmode() {
 		return wmode;
 	}
-
 	public void setWmode(int wmode) {
 		this.wmode = wmode;
 	}
-
 	/*public Object[] builderProperties() {
 		Object[] properties = new Object[1];
 		properties[0] = (WifiDeviceHelper.SwitchMode_Router2Bridge == wmode)?WifiDeviceHelper.WorkMode_Bridge:WifiDeviceHelper.WorkMode_Router;
 		return properties;
 	}*/
-	
 	public static void main(String[] argv){
 		//http://7xpatx.dl1.z0.glb.clouddn.com/uRouter/smb.package.tar.gz
 		ParamVasSwitchWorkmodeDTO dto = new ParamVasSwitchWorkmodeDTO();
