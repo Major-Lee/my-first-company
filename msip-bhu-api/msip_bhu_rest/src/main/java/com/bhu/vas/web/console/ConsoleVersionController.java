5813d1c71ccfd4a26ad1ff39c52776096e60cf8c
NeeBie-xw <476814114@qq.com>
2016-01-12 15:06:52 +0800

    文件上传

--- msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/console/ConsoleVersionController.java
+++ msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/console/ConsoleVersionController.java
@@ -1,6 +1,9 @@
 package com.bhu.vas.web.console;
 
+import java.io.File;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -28,146 +31,158 @@ import com.smartwork.msip.jdo.ResponseSuccess;
 @RequestMapping("/console/ver")
 public class ConsoleVersionController extends BaseController {
 
-    @Resource
-    private IVapRpcService vapRpcService;
+	@Resource
+	private IVapRpcService vapRpcService;
+	@Resource
+	private YunUploadService yunUploadService;
 
-    /**
-     * 获取设备定义的类型
-     *
-     * @param request
-     * @param response
-     */
-    @ResponseBody()
-    @RequestMapping(value = "/fetch_duts", method = {RequestMethod.POST})
-    public void fetch_duts(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            @RequestParam(required = true) int uid) {
-    	RpcResponseDTO<List<DeviceUnitTypeVTO>> rpcResult = vapRpcService.deviceUnitTypes(uid);
-		if(!rpcResult.hasError())
+	/**
+	 * 获取设备定义的类型
+	 *
+	 * @param request
+	 * @param response
+	 */
+	@ResponseBody()
+	@RequestMapping(value = "/fetch_duts", method = { RequestMethod.POST })
+	public void fetch_duts(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(required = true) int uid) {
+		RpcResponseDTO<List<DeviceUnitTypeVTO>> rpcResult = vapRpcService.deviceUnitTypes(uid);
+		if (!rpcResult.hasError())
 			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
 		else
 			SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
-    }
+	}
 
-    /**
+	/**
+	 * 
+	 * 获取设备类型的灰度列表
+	 * 
+	 * @param request
+	 * @param response
+	 * @param uid
+	 * @param dut
+	 */
+	@ResponseBody()
+	@RequestMapping(value = "/fetch_grays", method = { RequestMethod.POST })
+	public void fetch_grays(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(required = true) int uid, @RequestParam(required = true) String dut) {
+		RpcResponseDTO<CurrentGrayUsageVTO> rpcResult = vapRpcService.currentGrays(uid, dut);
+		if (!rpcResult.hasError())
+			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
+		else
+			SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
+	}
 
-     * 获取设备类型的灰度列表
-     * @param request
-     * @param response
-     * @param uid
-     * @param dut
-     */
-    @ResponseBody()
-    @RequestMapping(value = "/fetch_grays", method = {RequestMethod.POST})
-    public void fetch_grays(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            @RequestParam(required = true) int uid,
-            @RequestParam(required = true) String dut) {
-    	RpcResponseDTO<CurrentGrayUsageVTO> rpcResult = vapRpcService.currentGrays(uid, dut);
-		if(!rpcResult.hasError())
+	/**
+	 * 获取指定设备型号的固件版本分页列表或者增值模块版本分页列表
+	 * 
+	 * @param request
+	 * @param response
+	 */
+	@ResponseBody()
+	@RequestMapping(value = "/pagesdv", method = { RequestMethod.POST })
+	public void pagesdv(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(required = true) int uid, @RequestParam(required = true) String dut,
+			@RequestParam(required = true) boolean fw,
+			@RequestParam(required = false, defaultValue = "1", value = "pn") int pageNo,
+			@RequestParam(required = false, defaultValue = "10", value = "ps") int pageSize) {
+		RpcResponseDTO<TailPage<VersionVTO>> rpcResult = vapRpcService.pagesDeviceVersions(uid, dut, fw, pageNo,
+				pageSize);
+		if (!rpcResult.hasError())
 			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
 		else
 			SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
-    }
+	}
 
-    /**
-     * 获取指定设备型号的固件版本分页列表或者增值模块版本分页列表
-     * @param request
-     * @param response
-     */
-    @ResponseBody()
-    @RequestMapping(value = "/pagesdv", method = {RequestMethod.POST})
-    public void pagesdv(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            @RequestParam(required = true) int uid,
-            @RequestParam(required = true) String dut,
-            @RequestParam(required = true) boolean fw,
-            @RequestParam(required = false, defaultValue = "1", value = "pn") int pageNo,
-            @RequestParam(required = false, defaultValue = "10", value = "ps") int pageSize
-    		) {
-		RpcResponseDTO<TailPage<VersionVTO>> rpcResult = vapRpcService.pagesDeviceVersions(uid, dut, fw, pageNo, pageSize);
-		if(!rpcResult.hasError())
+	/**
+	 * 变更指定产品类型的灰度关联的固件版本号和增值组件版本号
+	 * 
+	 * @param request
+	 * @param response
+	 * @param uid
+	 * @param dut
+	 * @param gl
+	 * @param fwid
+	 * @param omid
+	 */
+	@ResponseBody()
+	@RequestMapping(value = "/modifyrv4gv", method = { RequestMethod.POST })
+	public void modifyrv4gv(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(required = true) int uid, @RequestParam(required = true) String dut,
+			@RequestParam(required = true) int gl, @RequestParam(required = true) String fwid,
+			@RequestParam(required = true) String omid) {
+		RpcResponseDTO<GrayUsageVTO> rpcResult = vapRpcService.modifyRelatedVersion4GrayVersion(uid, dut, gl, fwid,
+				omid);
+		if (!rpcResult.hasError())
 			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
 		else
 			SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
-    }
-    
-    /**
-     * 变更指定产品类型的灰度关联的固件版本号和增值组件版本号
-     * @param request
-     * @param response
-     * @param uid
-     * @param dut
-     * @param gl
-     * @param fwid
-     * @param omid
-     */
-    @ResponseBody()
-    @RequestMapping(value = "/modifyrv4gv", method = {RequestMethod.POST})
-    public void modifyrv4gv(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            @RequestParam(required = true) int uid,
-            @RequestParam(required = true) String dut,
-            @RequestParam(required = true) int gl,
-            @RequestParam(required = true) String fwid,
-            @RequestParam(required = true) String omid
-    		) {
-		RpcResponseDTO<GrayUsageVTO> rpcResult = vapRpcService.modifyRelatedVersion4GrayVersion(uid, dut, gl, fwid, omid);
-		if(!rpcResult.hasError())
+	}
+
+	/**
+	 * 增加固件版本或者增值组件版本信息
+	 * 
+	 * @param request
+	 * @param response
+	 * @param uid
+	 * @param dut
+	 * @param fw
+	 * @param versionid
+	 * @param upgrade_url
+	 */
+	@ResponseBody()
+	@RequestMapping(value = "/adddv", method = { RequestMethod.POST })
+	public void adddv(
+			HttpServletRequest request, 
+			HttpServletResponse response, 
+			@RequestParam("file") File file,
+			@RequestParam(required = true) int uid, 
+			@RequestParam(required = true) String dut,
+			@RequestParam(required = true) boolean fw) {
+		
+		uploadYun(file);
+		String QNurl = yunUploadService.QN_BUCKET_URL+file.getName();
+		String ALurl = yunUploadService.AL_BUCKET_NAME+"."+yunUploadService.AL_END_POINT+"/"+yunUploadService.AL_REMATE_NAME+file.getName();
+		RpcResponseDTO<VersionVTO> rpcResult = vapRpcService.addDeviceVersion(uid, dut, fw, file.getName(),QNurl,ALurl);
+		if (!rpcResult.hasError())
 			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
 		else
 			SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
-    }
-    
-    /**
-     * 增加固件版本或者增值组件版本信息
-     * @param request
-     * @param response
-     * @param uid
-     * @param dut
-     * @param fw
-     * @param versionid
-     * @param upgrade_url
-     */
-    @ResponseBody()
-    @RequestMapping(value = "/adddv", method = {RequestMethod.POST})
-    public void adddv(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            @RequestParam(required = true) int uid,
-            @RequestParam(required = true) String dut,
-            @RequestParam(required = true) boolean fw,
-            @RequestParam(required = true) String versionid, 
-            @RequestParam(required = true) String upgrade_url,
-            @RequestParam(required = false) String upgrade_slaver_urls
-            ) {
-    	RpcResponseDTO<VersionVTO> rpcResult = vapRpcService.addDeviceVersion(uid, dut, fw, versionid, upgrade_url,upgrade_slaver_urls);
-		if(!rpcResult.hasError())
+
+	}
+
+	private ExecutorService exec = Executors.newFixedThreadPool(2);
+
+	// 异步的上传至阿里云、七牛云
+	private void uploadYun(final File file) {
+		
+		exec.submit((new Runnable() {
+			@Override
+			public void run() {
+				
+				try {
+					// 阿里云
+					yunUploadService.uploadFile(file,yunUploadService.AL_REMATE_NAME);
+					// 七牛云
+					yunUploadService.uploadFile(file, yunUploadService.QN_REMATE_NAME, yunUploadService.QN_bucket_name);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		}));
+	}
+
+	@ResponseBody()
+	@RequestMapping(value = "/removedv", method = { RequestMethod.POST })
+	public void removedv(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(required = true) int uid, @RequestParam(required = true) String dut,
+			@RequestParam(required = true) boolean fw, @RequestParam(required = true) String versionid) {
+		RpcResponseDTO<VersionVTO> rpcResult = vapRpcService.removeDeviceVersion(uid, dut, fw, versionid);
+		if (!rpcResult.hasError())
 			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
 		else
 			SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
-    }
-    
-    
-    @ResponseBody()
-    @RequestMapping(value = "/removedv", method = {RequestMethod.POST})
-    public void removedv(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            @RequestParam(required = true) int uid,
-            @RequestParam(required = true) String dut,
-            @RequestParam(required = true) boolean fw,
-            @RequestParam(required = true) String versionid
-            ) {
-    	RpcResponseDTO<VersionVTO> rpcResult = vapRpcService.removeDeviceVersion(uid, dut, fw, versionid);
-		if(!rpcResult.hasError())
-			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
-		else
-			SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
-    }
-    
+	}
+
 }
