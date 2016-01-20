80b7502954ec06a3748a9f9674c29716438db8cc
Edmond Lee <wenhualee@163.com>
2016-01-20 20:34:42 +0800

    add 4

--- msip-bhu-unit/msip_bhu_unit_devices/src/test/java/com/bhu/vas/rpc/facade/DeviceRestBusinessFacadeServiceTest.java
+++ msip-bhu-unit/msip_bhu_unit_devices/src/test/java/com/bhu/vas/rpc/facade/DeviceRestBusinessFacadeServiceTest.java
@@ -50,5 +50,7 @@ public class DeviceRestBusinessFacadeServiceTest extends BaseTest{
 		System.out.println(1);
 		System.out.println(2);
 		System.out.println(3);
+		System.out.println(4);
+		
 	}
 }
