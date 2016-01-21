84dbac2120776e85819114a33415d3ba8c317fb0
Edmond Lee <wenhualee@163.com>
2016-01-21 09:25:10 +0800

    uRouter app 登录会进行token重置

--- msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/facade/UserUnitFacadeService.java
+++ msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/facade/UserUnitFacadeService.java
@@ -258,7 +258,7 @@ public class UserUnitFacadeService {
 			}
 			this.userService.update(user);
 			
-			uToken = userTokenService.generateUserAccessToken(user.getId().intValue(), true, false);
+			uToken = userTokenService.generateUserAccessToken(user.getId().intValue(), true, true);
 			{//write header to response header
 				//BusinessWebHelper.setCustomizeHeader(response, uToken);
 				IegalTokenHashService.getInstance().userTokenRegister(user.getId().intValue(), uToken.getAtoken());
