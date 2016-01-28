d5c73d4cc4b1dad2d203311f5797bced56042be0
唐子超 <tangzichao@tangzichaodeMacBook-Pro.local>
2016-01-28 11:23:03 +0800

    增加根据uid和dut来搜索数据的业务方法

--- msip-bhu-business-impl/msip_bhu_business_springdata_search/src/main/java/com/bhu/vas/business/search/service/WifiDeviceDataSearchService.java
+++ msip-bhu-business-impl/msip_bhu_business_springdata_search/src/main/java/com/bhu/vas/business/search/service/WifiDeviceDataSearchService.java
@@ -1,5 +1,8 @@
 package com.bhu.vas.business.search.service;
 
+import java.util.Collections;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.elasticsearch.common.lang3.StringUtils;
@@ -7,6 +10,10 @@ import org.springframework.stereotype.Service;
 
 import com.bhu.vas.business.search.BusinessIndexDefine;
 import com.bhu.vas.business.search.core.condition.AbstractDataSearchConditionService;
+import com.bhu.vas.business.search.core.condition.component.SearchCondition;
+import com.bhu.vas.business.search.core.condition.component.SearchConditionMessage;
+import com.bhu.vas.business.search.core.condition.component.SearchConditionPack;
+import com.bhu.vas.business.search.core.condition.component.SearchConditionPattern;
 import com.bhu.vas.business.search.core.field.FieldDefine;
 import com.bhu.vas.business.search.model.WifiDeviceDocument;
 import com.bhu.vas.business.search.repository.WifiDeviceDocumentRepository;
@@ -36,25 +43,25 @@ public class WifiDeviceDataSearchService extends AbstractDataSearchConditionServ
 	 * 设备的业务线类型
 	 * @param u_id
 	 * @param d_dut
-	 * @param d_
 	 * @return
 	 */
-/*	public Page<WifiDeviceDocument> searchPageByUidAndDut(Integer u_id, String d_dut, int pageNo, int pageSize){
-		if(u_id == null || StringUtils.isEmpty(d_dut)) return null;
+	public List<WifiDeviceDocument> searchListByUidAndDut(Integer u_id, String d_dut){
+		if(u_id == null) return Collections.emptyList();
 		
-//		SearchCondition sc_d_dut = new SearchCondition(BusinessIndexDefine.WifiDevice.
-//				Field.D_DEVICEUNITTYPE.getName(), SearchConditionPattern.StringEqual.getPattern(), d_dut);
-//		SearchCondition sc_u_id = new SearchCondition(BusinessIndexDefine.WifiDevice.
-//				Field.U_ID.getName(), SearchConditionPattern.StringEqual.getPattern(), String.valueOf(u_id));
+		SearchConditionPack pack_must = null;
 		
-		SearchCondition sc_d_dut = SearchCondition.builderSearchCondition(BusinessIndexDefine.WifiDevice.
-				Field.D_DEVICEUNITTYPE.getName(), SearchConditionPattern.StringEqual.getPattern(), d_dut);
 		SearchCondition sc_u_id = SearchCondition.builderSearchCondition(BusinessIndexDefine.WifiDevice.
 				Field.U_ID.getName(), SearchConditionPattern.StringEqual.getPattern(), String.valueOf(u_id));
 		
-		SearchConditionPack pack_must = SearchConditionPack.builderSearchConditionPackWithConditions(sc_d_dut, sc_u_id);
-		
+		if(StringUtils.isNotEmpty(d_dut)){
+			SearchCondition sc_d_dut = SearchCondition.builderSearchCondition(BusinessIndexDefine.WifiDevice.
+					Field.D_DEVICEUNITTYPE.getName(), SearchConditionPattern.StringEqual.getPattern(), d_dut);
+			pack_must = SearchConditionPack.builderSearchConditionPackWithConditions(sc_d_dut, sc_u_id);
+		}else{
+			pack_must = SearchConditionPack.builderSearchConditionPackWithConditions(sc_u_id);
+		}
+
 		SearchConditionMessage scm = SearchConditionMessage.builderSearchConditionMessage(pack_must);
-		return super.searchByConditionMessage(scm, pageNo, pageSize);
-	}*/
+		return super.searchByConditionMessage(scm);
+	}
 }
