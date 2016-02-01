3f48f8caa0f165053a8a3c0cab59fc9b81402d48
Edmond Lee <wenhualee@163.com>
2016-02-01 13:50:23 +0800

    pid -> pId 并增加isparent字段，兼容ztree客户端页面插件的格式

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/DeviceGroupVTO.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/DeviceGroupVTO.java
@@ -5,7 +5,7 @@ package com.bhu.vas.api.vto;
 public class DeviceGroupVTO implements java.io.Serializable{
 	private long gid;
 
-	private long pid;
+	private long pId;
 	
 	private String name;
 
@@ -15,6 +15,8 @@ public class DeviceGroupVTO implements java.io.Serializable{
 
 	private int children;
 
+	private boolean isparent;
+	
 	private int device_count;
 
 	//private TailPage<WifiDeviceVTO> page_devices;
@@ -25,14 +27,6 @@ public class DeviceGroupVTO implements java.io.Serializable{
 
 	public void setGid(long gid) {
 		this.gid = gid;
-	}
-
-	public long getPid() {
-		return pid;
-	}
-
-	public void setPid(long pid) {
-		this.pid = pid;
 	}
 
 	public String getName() {
@@ -73,6 +67,22 @@ public class DeviceGroupVTO implements java.io.Serializable{
 
 	public void setChildren(int children) {
 		this.children = children;
+	}
+
+	public long getpId() {
+		return pId;
+	}
+
+	public void setpId(long pId) {
+		this.pId = pId;
+	}
+
+	public boolean isIsparent() {
+		return children>0;
+	}
+
+	public void setIsparent(boolean isparent) {
+		this.isparent = isparent;
 	}
 
 	/*public TailPage<WifiDeviceVTO> getPage_devices() {
