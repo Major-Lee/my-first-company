32d4bca3567dc0ab83de0b0367cf136d7c5a3c99
Edmond Lee <wenhualee@163.com>
2016-02-26 14:00:34 +0800

    fixed

--- msip-bhu-unit/msip_bhu_unit_input_processor/src/main/java/com/bhu/vas/rpc/consumer/MessageInputConsumer.java
+++ msip-bhu-unit/msip_bhu_unit_input_processor/src/main/java/com/bhu/vas/rpc/consumer/MessageInputConsumer.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MessageInputConsumer {
 	public static void main(String[] args) throws Exception {
 		System.setProperty("appname", "BHUInputRpcConsumerApp");
-		System.setProperty("zookeeper", "192.168.66.191:2181");
+		//System.setProperty("zookeeper", "192.168.66.191:2181");
 		System.setProperty("provider.port", "");
 		//System.setProperty("provider.port", "20882");
 		//System.out.println("~~~~~~~~~~~~~:"+System.getProperty("provider.port"));
