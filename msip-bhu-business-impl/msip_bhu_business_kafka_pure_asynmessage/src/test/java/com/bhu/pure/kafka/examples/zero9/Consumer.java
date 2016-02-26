b81195be20a8b1c1de770740d6c91e7cc8caf502
唐子超 <tangzichao@tangzichaodeMacBook-Pro.local>
2016-02-26 14:05:23 +0800

    added

--- msip-bhu-business-impl/msip_bhu_business_kafka_pure_asynmessage/src/test/java/com/bhu/pure/kafka/examples/zero9/Consumer.java
+++ msip-bhu-business-impl/msip_bhu_business_kafka_pure_asynmessage/src/test/java/com/bhu/pure/kafka/examples/zero9/Consumer.java
@@ -35,7 +35,7 @@ public class Consumer extends ShutdownableThread
     super("KafkaConsumerExample", false);
     Properties props = new Properties();
     //props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.66.123:9092");
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.66.123:9092,192.168.66.123:9093");
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.170.197.250:9092,10.170.197.250:9093,10.173.33.85:9092,10.173.33.85:9093");
     props.put(ConsumerConfig.GROUP_ID_CONFIG, "DemoConsumer");
     props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
     props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
