e4559ae1dabc52ec49481962473ad42935c843dc
Edmond Lee <wenhualee@163.com>
2016-03-01 20:57:50 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/user/model/UserWalletLog.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/user/model/UserWalletLog.java
@@ -12,11 +12,11 @@ import com.smartwork.msip.cores.orm.model.BaseIntModel;
 public class UserWalletLog extends BaseIntModel{// implements ISequenceGenable,TableSplitable<Integer>{
 	private int uid;
 	//交易类别 充值购买虎钻 充值现金 系统/活动赠送虎钻 消费虎钻 提现(withdraw)
-	private int transaction;
+	private String transaction;
 	//transaction 描述 用于查看方便的冗余字段 
 	private String transaction_desc;
-	//交易数量(正负数字 充值购买虎钻 系统/活动赠送虎钻 消费虎钻 时值不为0) 
-	private Double sum;
+	//交易数量(正负数字 充值购买虎钻 系统/活动赠送虎钻 消费虎钻 时 值不为0) 
+	private int sum;
 	//现金（正负数字 充值购买虎钻 充值现金 提现(withdraw)） 
 	private Double amount;
 	//交易内容描述
@@ -35,10 +35,10 @@ public class UserWalletLog extends BaseIntModel{// implements ISequenceGenable,T
 		this.uid = uid;
 	}
 	
-	public int getTransaction() {
+	public String getTransaction() {
 		return transaction;
 	}
-	public void setTransaction(int transaction) {
+	public void setTransaction(String transaction) {
 		this.transaction = transaction;
 	}
 	public String getTransaction_desc() {
@@ -46,12 +46,6 @@ public class UserWalletLog extends BaseIntModel{// implements ISequenceGenable,T
 	}
 	public void setTransaction_desc(String transaction_desc) {
 		this.transaction_desc = transaction_desc;
-	}
-	public Double getSum() {
-		return sum;
-	}
-	public void setSum(Double sum) {
-		this.sum = sum;
 	}
 	public Double getAmount() {
 		return amount;
@@ -65,4 +59,12 @@ public class UserWalletLog extends BaseIntModel{// implements ISequenceGenable,T
 	public void setDesc(String desc) {
 		this.desc = desc;
 	}
+	public int getSum() {
+		return sum;
+	}
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+	
+	
 }
