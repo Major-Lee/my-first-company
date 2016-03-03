c7c71ddb39bafa0d88dd1ee21c54db983cc994d2
Edmond Lee <wenhualee@163.com>
2016-03-03 22:55:45 +0800

    增加接口 钱包 提现和详情接口

---
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/wallet/UserWalletDetailVTO.java
@@ -0,0 +1,46 @@
+package com.bhu.vas.api.vto.wallet;
+
+/**
+ * 
+ * @author Edmond
+ *
+ */
+@SuppressWarnings("serial")
+public class UserWalletDetailVTO implements java.io.Serializable {
+	private int uid;
+	private double vcurrency = 0.00d;
+	private double vcurrency_bing = 0.00d;
+	private double cash = 0.00d;;
+	private boolean withdraw = false;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public double getCash() {
+		return cash;
+	}
+	public void setCash(double cash) {
+		this.cash = cash;
+	}
+	public double getVcurrency() {
+		return vcurrency;
+	}
+	public void setVcurrency(double vcurrency) {
+		this.vcurrency = vcurrency;
+	}
+	public double getVcurrency_bing() {
+		return vcurrency_bing;
+	}
+	public void setVcurrency_bing(double vcurrency_bing) {
+		this.vcurrency_bing = vcurrency_bing;
+	}
+	public boolean isWithdraw() {
+		return withdraw;
+	}
+	public void setWithdraw(boolean withdraw) {
+		this.withdraw = withdraw;
+	}
+	
+}
---
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/wallet/UserWithdrawApplyVTO.java
@@ -0,0 +1,55 @@
+package com.bhu.vas.api.vto.wallet;
+
+/**
+ * 
+ * @author Edmond
+ *
+ */
+@SuppressWarnings("serial")
+public class UserWithdrawApplyVTO implements java.io.Serializable {
+	private long applyid;
+	private int uid;
+	private String mobileno;
+	private String nick;
+	private double cash = 0.00d;
+	//当前提现申请的状态
+	private String withdraw_oper;
+	
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public double getCash() {
+		return cash;
+	}
+	public void setCash(double cash) {
+		this.cash = cash;
+	}
+	public String getWithdraw_oper() {
+		return withdraw_oper;
+	}
+	public void setWithdraw_oper(String withdraw_oper) {
+		this.withdraw_oper = withdraw_oper;
+	}
+	public long getApplyid() {
+		return applyid;
+	}
+	public void setApplyid(long applyid) {
+		this.applyid = applyid;
+	}
+	
+}
