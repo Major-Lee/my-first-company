585d7c65235155e6cf0cc869984b68fa1bc2a1c1
meng008 <banzhiyancool@126.com>
2016-03-07 09:07:45 +0800

    11111

--- msip-bhu-business-impl/msip_bhu_business_ds_social/src/test/java/com/bhu/vas/business/ds/social/WifiCommentTest.java
+++ msip-bhu-business-impl/msip_bhu_business_ds_social/src/test/java/com/bhu/vas/business/ds/social/WifiCommentTest.java
@@ -2,11 +2,13 @@ package com.bhu.vas.business.ds.social;
 
 import com.bhu.vas.api.rpc.social.model.WifiComment;
 import com.bhu.vas.business.ds.social.service.WifiCommentService;
+import com.smartwork.msip.cores.orm.support.criteria.ModelCriteria;
 import com.smartwork.msip.localunit.BaseTest;
 import org.junit.Test;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by bluesand on 3/2/16.
@@ -31,5 +33,19 @@ public class WifiCommentTest extends BaseTest {
         wifiCommentService.insert(wifiComment);
 
 
+    }
+    
+    
+    @Test 
+    public void findModelByCommonCriteria(){
+    	
+    	ModelCriteria mc = new ModelCriteria();
+        mc.createCriteria().andSimpleCaulse("1=1").andColumnEqualTo("bssid", "1234");
+        int total = wifiCommentService.countByCommonCriteria(mc);
+        mc.setPageNumber(0);
+        mc.setPageSize(5);
+        mc.setOrderByClause(" created_at desc");
+
+        List<WifiComment> wifiComments = wifiCommentService.findModelByCommonCriteria(mc);
     }
 }
