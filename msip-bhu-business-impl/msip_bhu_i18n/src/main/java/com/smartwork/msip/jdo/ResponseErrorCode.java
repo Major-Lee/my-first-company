0817dbe965cfd5982f3897976e5b9b4463f32b67
NeeBie-xw <476814114@qq.com>
2016-03-17 17:30:13 +0800

    fix

--- msip-bhu-business-impl/msip_bhu_i18n/src/main/java/com/smartwork/msip/jdo/ResponseErrorCode.java
+++ msip-bhu-business-impl/msip_bhu_i18n/src/main/java/com/smartwork/msip/jdo/ResponseErrorCode.java
@@ -213,8 +213,8 @@ public enum ResponseErrorCode {
 	REQUEST_UNKNOW_ERROR("599","unknow.pagerequest.error"),
 
 	//Social
-	SOCIAL_FOLLOW_ERROR("1100","1100.follow.error"),
-	SOCIAL_FOLLOW_MAX("1101","1101.follow.isMAX.error");
+	SOCIAL_FOLLOW_ERROR("1100","follow.error"),
+	SOCIAL_FOLLOW_MAX("1101","follow.max.error");
 
     private String code;
     private String i18n;
