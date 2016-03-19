b1405b957363272ccb56296ce8fe1a3fad9f7ca5
Edmond Lee <wenhualee@163.com>
2016-03-19 14:34:08 +0800

    added

---
+++ msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/device/DeviceSharenetworkController.java
@@ -0,0 +1,102 @@
+package com.bhu.vas.web.device;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bhu.vas.api.dto.redis.DeviceUsedStatisticsDTO;
+import com.bhu.vas.api.helper.VapEnumType;
+import com.bhu.vas.api.helper.VapEnumType.SharedNetworkType;
+import com.bhu.vas.api.rpc.RpcResponseDTO;
+import com.bhu.vas.api.rpc.devices.dto.sharednetwork.SharedNetworkVTO;
+import com.bhu.vas.api.rpc.devices.iservice.IDeviceURouterRestRpcService;
+import com.bhu.vas.api.rpc.user.dto.UserTerminalOnlineSettingDTO;
+import com.bhu.vas.api.vto.URouterAdminPasswordVTO;
+import com.bhu.vas.api.vto.URouterEnterVTO;
+import com.bhu.vas.api.vto.URouterHdDetailVTO;
+import com.bhu.vas.api.vto.URouterHdHostNameVTO;
+import com.bhu.vas.api.vto.URouterMainEnterVTO;
+import com.bhu.vas.api.vto.URouterModeVTO;
+import com.bhu.vas.api.vto.URouterPeakSectionsDTO;
+import com.bhu.vas.api.vto.URouterRealtimeRateVTO;
+import com.bhu.vas.api.vto.URouterSettingVTO;
+import com.bhu.vas.api.vto.URouterVapPasswordVTO;
+import com.bhu.vas.api.vto.config.URouterDeviceConfigVTO;
+import com.bhu.vas.msip.cores.web.mvc.spring.BaseController;
+import com.bhu.vas.msip.cores.web.mvc.spring.helper.SpringMVCHelper;
+import com.smartwork.msip.jdo.ResponseError;
+import com.smartwork.msip.jdo.ResponseSuccess;
+
+@Controller
+@RequestMapping("/device/sharenetwork")
+public class DeviceSharenetworkController extends BaseController{
+	//@Resource
+	//private IDeviceURouterRestRpcService deviceURouterRestRpcService;
+	
+	@ResponseBody()
+	@RequestMapping(value="/supported",method={RequestMethod.POST})
+	public void supported(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			@RequestParam(required = true) Integer uid,
+			@RequestParam(required = true) String mac) {
+		SpringMVCHelper.renderJson(response, ResponseSuccess.embed(SharedNetworkType.getSharedNetworkVtos()));
+	}
+	
+	/**
+	 * 修改用户共享网络配置并应用接口
+	 * @param request
+	 * @param response
+	 * @param uid
+	 * @param sharenetwork_type
+	 * @param mac
+	 */
+	@ResponseBody()
+	@RequestMapping(value="/apply",method={RequestMethod.POST})
+	public void apply(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			@RequestParam(required = true) Integer uid,
+			@RequestParam(required = false,defaultValue= "SafeSecure",value="snt") String sharenetwork_type,
+			@RequestParam(required = false) String extparams) {
+		/*RpcResponseDTO<URouterMainEnterVTO> rpcResult = deviceURouterRestRpcService.urouterMainEnter(uid, mac.toLowerCase());
+		if(!rpcResult.hasError()){
+			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
+		}else{
+			SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
+		}*/
+	}
+	
+	/**
+	 * 获取用户所属指定共享网络配置的设备分页列表
+	 * @param request
+	 * @param response
+	 * @param uid
+	 * @param sharenetwork_type
+	 */
+	@ResponseBody()
+	@RequestMapping(value="/pagedevices",method={RequestMethod.POST})
+	public void pagedevices(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			@RequestParam(required = true) Integer uid,
+			@RequestParam(required = false,defaultValue= "SafeSecure",value="snt") String sharenetwork_type) {
+		
+		/*RpcResponseDTO<URouterEnterVTO> rpcResult = deviceURouterRestRpcService.urouterEnter(uid, mac.toLowerCase());
+		if(!rpcResult.hasError()){
+			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
+		}else{
+			SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
+		}*/
+	}
+	
+}
