3c93659d61e76d4fdf3fc6720ddd90f53843ed6e
Edmond Lee <wenhualee@163.com>
2016-03-31 18:04:31 +0800

    去除空格的转义字符

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/helper/WifiDeviceHelper.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/helper/WifiDeviceHelper.java
@@ -420,7 +420,7 @@ public class WifiDeviceHelper {
                 txt = HtmlHelper.replace(txt, "<", "&lt;");
                 txt = HtmlHelper.replace(txt, "&amp;gt;", "&gt;");
                 txt = HtmlHelper.replace(txt, ">", "&gt;");
-                txt = HtmlHelper.replace(txt, "&amp;nbsp;", "&nbsp;");
+                //txt = HtmlHelper.replace(txt, "&amp;nbsp;", "&nbsp;");
                 //txt = HtmlHelper.replace(txt, " ", "&nbsp;");
                 txt = HtmlHelper.replace(txt, "&amp;#8217;", "&#8217;");
                 txt = HtmlHelper.replace(txt, "'", "&#8217;");
