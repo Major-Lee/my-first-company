386d085a8ad270eac68b36f23bfbbddd8bcbf3ea
Edmond Lee <wenhualee@163.com>
2016-04-12 11:36:06 +0800

    added

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/charging/model/WifiDeviceSharedealConfigs.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/charging/model/WifiDeviceSharedealConfigs.java
@@ -5,18 +5,31 @@ import java.util.Date;
 import com.smartwork.msip.cores.orm.model.BaseStringModel;
 
 /**
+ * 方式一：需要考虑
+ * 非导入的设备不在此表中存在记录
+ * 不在此表中的设备缺省应用Default_ConfigsWifiID对应的设备并不新增
+ * 		处理逻辑：A、getById发现此设备不存在，则return getById(Default_ConfigsWifiID)
+ * 				B、getById发现此设备存在，则return
+ * 				C、owner字段最好动态获取（每次读比较耗资源）
+ * 
+ * 方式二：需要考虑 
+ * 不在此表中的设备缺省应用Default_ConfigsWifiID对应的配置并新增到此表中
+ * 需要维护owner字段的变更：
+ * 	a、解绑、绑定、重置对owner字段的影响，解绑和重置owner值变为-1
+ * 	b、这样owner字段被维护好后
  * @author Edmond
  *
  */
 @SuppressWarnings("serial")
 public class WifiDeviceSharedealConfigs extends BaseStringModel{
 	public static final String Default_ConfigsWifiID = "00:00:00:00:00:00";
-	public static final String Default_Range_Cash = "1.5-3.5";
+	public static final String Default_Range_Cash_PC = "1.5-3.5";
+	public static final String Default_Range_Cash_Mobile = "0.5-0.9";
 	public static final String Default_AIT = "14400";
 	//如果owner字段为<=0则采用此值为缺省分成用户
-	public static final String Default_Owner = "1";
+	public static final int Default_Owner = 1;
 	//如果manufacturer<=0字段为空则采用此值为缺省分成用户
-	public static final String Default_Manufacturer = "110";
+	public static final int Default_Manufacturer = 110;
 	//设备批次号（导入批次号-一般库房进行编号）格式为 yyyyMMdd-longsequence
 	private String batchno;
 	//绑定用户 <=0 代表未绑定的设备（此数据为冗余数据，解绑和绑定、重置操作都需要操作此值）
@@ -28,8 +41,8 @@ public class WifiDeviceSharedealConfigs extends BaseStringModel{
 	private double manufacturer_percent = 0.30d;
 	
 	//pc端和移动端的打赏金额范围及相关时长
-	private String range_cash_pc = Default_Range_Cash;
-	private String range_cash_mobile = Default_Range_Cash;
+	private String range_cash_pc = Default_Range_Cash_PC;
+	private String range_cash_mobile = Default_Range_Cash_Mobile;
 	//acessInternettime ait
 	private String ait_pc = Default_AIT;
 	private String ait_mobile = Default_AIT;
