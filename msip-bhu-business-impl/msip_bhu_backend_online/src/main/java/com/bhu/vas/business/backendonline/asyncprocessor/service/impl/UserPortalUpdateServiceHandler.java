fb5b91e28d74e5701a1387b0bbf4563e1608ccb0
Edmond Lee <wenhualee@163.com>
2016-04-15 16:00:53 +0800

    移除 共享网络应用修改动作 后续通知backend 调用uportal的操作

--- msip-bhu-business-impl/msip_bhu_backend_online/src/main/java/com/bhu/vas/business/backendonline/asyncprocessor/service/impl/UserPortalUpdateServiceHandler.java
+++ msip-bhu-business-impl/msip_bhu_backend_online/src/main/java/com/bhu/vas/business/backendonline/asyncprocessor/service/impl/UserPortalUpdateServiceHandler.java
@@ -30,6 +30,16 @@ public class UserPortalUpdateServiceHandler implements IMsgHandlerService {
 	@Resource
 	private SharedNetworksFacadeService sharedNetworksFacadeService;
 	
+	private static Map<String,String> Common_Headers = new HashMap<String,String>();
+	static{
+		//headers.put(RuntimeConfiguration.Param_ATokenHeader, "OjJWVFJXAUEXT0UNCVFf");
+		Common_Headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+		Common_Headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36");
+				/*headers.put("X-Requested-With", "XMLHttpRequest");
+				headers.put("Accept-Encoding", "gzip, deflate");
+				headers.put("Accept-Language", "zh-CN,zh;q=0.8");
+				headers.put("Connection", "keep-alive");*/
+	}
 	@Override
 	public void process(String message) {
 		logger.info(String.format("process message[%s]", message));
@@ -53,7 +63,7 @@ public class UserPortalUpdateServiceHandler implements IMsgHandlerService {
 							}
 							logger.info(String.format("UserPortalUpdate2UPortalApi request url[%s] params[%s]", 
 									BusinessRuntimeConfiguration.UserPortalUpdate2UPortalApi, api_params));
-							String response = HttpHelper.postUrlAsString(BusinessRuntimeConfiguration.UserPortalUpdate2UPortalApi, api_params);
+							String response = HttpHelper.postUrlAsString(BusinessRuntimeConfiguration.UserPortalUpdate2UPortalApi, api_params,Common_Headers);
 							logger.info(String.format("UserPortalUpdate2UPortalApi Response url[%s] params[%s] response[%s]", 
 									BusinessRuntimeConfiguration.UserPortalUpdate2UPortalApi, api_params, response));
 						}catch(Exception ex){
@@ -96,4 +106,37 @@ public class UserPortalUpdateServiceHandler implements IMsgHandlerService {
 		wifiDeviceIndexIncrementService.wifiDeviceIndexBlukIncrement(wifiDeviceList, groupIdList);
 
 	}*/
+	
+	
+	public static void main(String[] argv) throws Exception{
+		String jsonMessage = "{\"uid\":3,\"ts\":1460627328403,\"type\":\"User\",\"nick\":\"熊出没了d\",\"mobileno\":\"18612272825\",\"avatar\":\"http://profile.icon\",\"snk\":null,\"tpl\":null,\"users_rate\":0,\"actionType\":\"UU\"}";
+		final UserPortalUpdateDTO dto = JsonHelper.getDTO(jsonMessage, UserPortalUpdateDTO.class);
+		final int uid = dto.getUid();
+		final String type = dto.getType();
+		Map<String, String> api_params = generateCommonApiParamMap(String.valueOf(uid));
+		/*if(UserPortalUpdateDTO.PortalUpdate_User.equals(type)){
+			api_params.put("title", dto.getNick());
+			api_params.put("mobileno", dto.getMobileno());
+			api_params.put("avatar", dto.getAvatar());
+		}else{
+			api_params.put("name", dto.getTpl());
+			api_params.put("snk", dto.getSnk());
+			api_params.put("rate", String.valueOf(dto.getUsers_rate()));
+		}*/
+		
+		Map<String,String> headers = new HashMap<String,String>();
+		//headers.put(RuntimeConfiguration.Param_ATokenHeader, "OjJWVFJXAUEXT0UNCVFf");
+		//headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+		headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36");
+		/*headers.put("X-Requested-With", "XMLHttpRequest");
+		headers.put("Accept-Encoding", "gzip, deflate");
+		headers.put("Accept-Language", "zh-CN,zh;q=0.8");
+		headers.put("Connection", "keep-alive");*/
+		
+		System.out.println(String.format("UserPortalUpdate2UPortalApi request url[%s] params[%s]", 
+				BusinessRuntimeConfiguration.UserPortalUpdate2UPortalApi, api_params));
+		String response = HttpHelper.postUrlAsString("http://192.168.66.91:8550/portal/set/recreate?uid=123456", api_params,headers,"UTF-8");
+		System.out.println(String.format("UserPortalUpdate2UPortalApi Response url[%s] params[%s] response[%s]", 
+				BusinessRuntimeConfiguration.UserPortalUpdate2UPortalApi, api_params, response));
+	}
 }
