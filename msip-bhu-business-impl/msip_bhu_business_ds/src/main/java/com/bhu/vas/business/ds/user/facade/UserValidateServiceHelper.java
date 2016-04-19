448a990846ec804888bf547df534637d60a65c9f
Edmond Lee <wenhualee@163.com>
2016-04-19 13:51:08 +0800

    访客网络给指定mac设备应用配置时判定此设备是否被此用户绑定 访客网络给提取指定mac设备配置时判定此设备是否被此用户绑定

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/facade/UserValidateServiceHelper.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/facade/UserValidateServiceHelper.java
@@ -1,7 +1,14 @@
 package com.bhu.vas.business.ds.user.facade;
 
+import java.util.List;
+
 import com.bhu.vas.api.rpc.user.model.User;
+import com.bhu.vas.api.rpc.user.model.UserDevice;
+import com.bhu.vas.api.rpc.user.model.pk.UserDevicePK;
+import com.bhu.vas.business.ds.user.service.UserDeviceService;
 import com.bhu.vas.business.ds.user.service.UserService;
+import com.smartwork.msip.business.runtimeconf.BusinessRuntimeConfiguration;
+import com.smartwork.msip.cores.orm.support.criteria.ModelCriteria;
 import com.smartwork.msip.exception.BusinessI18nCodeException;
 import com.smartwork.msip.jdo.ResponseErrorCode;
 
@@ -15,6 +22,45 @@ public class UserValidateServiceHelper {
 			throw new BusinessI18nCodeException(ResponseErrorCode.USER_DATA_NOT_EXIST);
 		}
 		return user;
+	}
+	
+	/**
+	 * console user 直接可以访问
+	 * @param uid
+	 * @param dmacs
+	 * @param userDeviceService
+	 */
+	public static void validateUserDevices(int uid,List<String> dmacs,UserDeviceService userDeviceService){
+		if(!BusinessRuntimeConfiguration.isConsoleUser(uid)){
+			ModelCriteria mc = new ModelCriteria();
+			mc.createCriteria().andSimpleCaulse(" 1=1 ").andColumnEqualTo("uid", uid).andColumnIn("mac", dmacs);
+			int count = userDeviceService.countByModelCriteria(mc);
+			if(dmacs.size() != count){
+				throw new BusinessI18nCodeException(ResponseErrorCode.DEVICE_NOT_YOURBINDED,new String[]{dmacs.toString()});
+			}
+		}
+	}
+	
+	/**
+	 * console user 直接可以访问
+	 * @param uid
+	 * @param dmac
+	 * @param userDeviceService
+	 * @return
+	 */
+	public static UserDevice validateUserDevice(int uid,String dmac,UserDeviceService userDeviceService){
+		if(uid <=0){
+			throw new BusinessI18nCodeException(ResponseErrorCode.USER_DATA_NOT_EXIST);
+		}
+		UserDevice userdevice_entity = null;
+		if(!BusinessRuntimeConfiguration.isConsoleUser(uid)){
+			//验证用户是否管理设备
+			userdevice_entity = userDeviceService.getById(new UserDevicePK(dmac, uid));
+			if(userdevice_entity == null){
+				throw new BusinessI18nCodeException(ResponseErrorCode.DEVICE_NOT_YOURBINDED,new String[]{dmac});
+			}
+		}
+		return userdevice_entity;
 	}
 	
 	//public abstract UserService getUserService();
