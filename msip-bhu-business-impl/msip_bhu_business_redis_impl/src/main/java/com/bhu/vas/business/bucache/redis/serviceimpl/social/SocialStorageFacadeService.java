31521d6f1daae1cd4ee2d0eb284b32e9f5a8f011
NeeBie-xw <476814114@qq.com>
2016-05-06 19:06:51 +0800

    log

--- msip-bhu-business-impl/msip_bhu_business_redis_impl/src/main/java/com/bhu/vas/business/bucache/redis/serviceimpl/social/SocialStorageFacadeService.java
+++ msip-bhu-business-impl/msip_bhu_business_redis_impl/src/main/java/com/bhu/vas/business/bucache/redis/serviceimpl/social/SocialStorageFacadeService.java
@@ -9,55 +9,62 @@ import java.util.List;
 /**
  * Created by bluesand on 3/11/16.
  */
-public class SocialStorageFacadeService  {
+public class SocialStorageFacadeService {
 
+	private final static int MAX_HANDSET_MEET_COUNT = 10;
 
-    private final static int MAX_HANDSET_MEET_COUNT = 10;
+	public static void handsetMeet(String uidOrhd_mac, String hd_mac, String bssid, String dto) {
 
+		List<HandsetMeetDTO> meets = getHandsetMeets(uidOrhd_mac, hd_mac, bssid);
 
-    public static void handsetMeet(String uidOrhd_mac, String hd_mac, String bssid, String dto) {
+		if (meets != null) {
+			if (meets.size() < MAX_HANDSET_MEET_COUNT && meets.size() >= 0) {
+				meets.add(0, JsonHelper.getDTO(dto, HandsetMeetDTO.class));
+			} else {
+				meets.add(0, JsonHelper.getDTO(dto, HandsetMeetDTO.class));
+				meets.remove(MAX_HANDSET_MEET_COUNT);
+			}
+		} else {
+			meets = new ArrayList<HandsetMeetDTO>();
+			meets.add(0, JsonHelper.getDTO(dto, HandsetMeetDTO.class));
+		}
 
-        List<HandsetMeetDTO> meets = getHandsetMeets(uidOrhd_mac, hd_mac, bssid);
+		SocialHandsetMeetHashService.getInstance().hsetHadsetMeets(uidOrhd_mac, hd_mac, bssid,
+				JsonHelper.getJSONString(meets));
 
-        if (meets != null) {
-            if (meets.size() < MAX_HANDSET_MEET_COUNT && meets.size() >= 0) {
-                meets.add(0, JsonHelper.getDTO(dto, HandsetMeetDTO.class));
-            } else {
-                meets.add(0, JsonHelper.getDTO(dto, HandsetMeetDTO.class));
-                meets.remove(MAX_HANDSET_MEET_COUNT);
-            }
-        } else {
-            meets = new ArrayList<HandsetMeetDTO>();
-            meets.add(0, JsonHelper.getDTO(dto, HandsetMeetDTO.class));
-        }
+		// SocialHandsetMeetHashService.getInstance().hincrbyHadsetMeetTotalWithBssid(uid,
+		// hd_mac, bssid);
+		SocialHandsetMeetHashService.getInstance().hincrbyHadsetMeetTotal(uidOrhd_mac, hd_mac);
 
-        SocialHandsetMeetHashService.getInstance().hsetHadsetMeets(uidOrhd_mac, hd_mac, bssid, JsonHelper.getJSONString(meets));
+		// SocialHandsetMeetHashService.getInstance().hsetLastMeetWithBssid(uid,
+		// hd_mac, bssid, dto);
+		SocialHandsetMeetHashService.getInstance().hsetLastMeet(uidOrhd_mac, hd_mac, dto);
 
-//        SocialHandsetMeetHashService.getInstance().hincrbyHadsetMeetTotalWithBssid(uid, hd_mac, bssid);
-        SocialHandsetMeetHashService.getInstance().hincrbyHadsetMeetTotal(uidOrhd_mac, hd_mac);
+	}
 
-//        SocialHandsetMeetHashService.getInstance().hsetLastMeetWithBssid(uid, hd_mac, bssid, dto);
-        SocialHandsetMeetHashService.getInstance().hsetLastMeet(uidOrhd_mac, hd_mac, dto);
+	public static List<HandsetMeetDTO> getHandsetMeets(String uid, String hd_mac, String bssid) {
+		String meets = SocialHandsetMeetHashService.getInstance().hgetHandsetMeets(uid, hd_mac, bssid);
+		System.out.println(meets);
+		return JsonHelper.getDTOList(meets, HandsetMeetDTO.class);
+	}
 
-    }
+	public static HandsetMeetDTO getLastHandsetMeetWithBssid(String hd_mac_self, String hd_mac, String bssid) {
+		String dto = SocialHandsetMeetHashService.getInstance().hgetLastHandsetMeetWithBssid(hd_mac_self, hd_mac,
+				bssid);
+		return JsonHelper.getDTO(dto, HandsetMeetDTO.class);
+	}
 
+	public static HandsetMeetDTO getLastHandsetMeet(String uid, String hd_mac) {
+		String dto = SocialHandsetMeetHashService.getInstance().hgetLastHandsetMeet(uid, hd_mac);
+		return JsonHelper.getDTO(dto, HandsetMeetDTO.class);
+	}
 
-    public static List<HandsetMeetDTO> getHandsetMeets(String uid, String hd_mac, String bssid) {
-        String meets = SocialHandsetMeetHashService.getInstance().hgetHandsetMeets(uid, hd_mac, bssid);
-        List<HandsetMeetDTO> list = new ArrayList<HandsetMeetDTO>();
-        list.add(JsonHelper.getDTO(meets,HandsetMeetDTO.class));
-        return list;
-    }
-
-    public static HandsetMeetDTO getLastHandsetMeetWithBssid(String hd_mac_self, String hd_mac, String bssid) {
-        String dto = SocialHandsetMeetHashService.getInstance().hgetLastHandsetMeetWithBssid(hd_mac_self, hd_mac, bssid);
-        return JsonHelper.getDTO(dto, HandsetMeetDTO.class);
-    }
-
-
-    public static HandsetMeetDTO getLastHandsetMeet(String uid, String hd_mac) {
-        String dto = SocialHandsetMeetHashService.getInstance().hgetLastHandsetMeet(uid, hd_mac);
-        return JsonHelper.getDTO(dto, HandsetMeetDTO.class);
-    }
+	public static void main(String[] args) {
+		String str = "[{\"bssid\":\"84:82:f4:28:7a:ed\",\"ssid\":\"bhuwifi-911ec\",\"ts\":1462530776123,\"lat\":\"40.018668\",\"lon\":\"116.351848\",\"addr\":\"aaa\"},{\"bssid\":null,\"ssid\":null,\"ts\":0,\"lat\":null,\"lon\":null,\"addr\":null}]";
+		List<HandsetMeetDTO> dtoList = JsonHelper.getDTOList(str, HandsetMeetDTO.class);
+		for(HandsetMeetDTO dto :dtoList){
+			System.out.println(dto.getSsid());
+		}
+	}
 
 }
