c66b80784a2b15fa5f4df788a48fee64d1d09942
Edmond Lee <wenhualee@163.com>
2016-05-09 19:25:42 +0800

    批量导入程序加上了 utype=50的验证

--- msip-bhu-business-impl/msip_bhu_dataimport/src/main/java/com/bhu/vas/di/op/batchimport/frommanufacturing/ManufacturerDeviceDataImportOP.java
+++ msip-bhu-business-impl/msip_bhu_dataimport/src/main/java/com/bhu/vas/di/op/batchimport/frommanufacturing/ManufacturerDeviceDataImportOP.java
@@ -19,12 +19,15 @@ import com.bhu.vas.di.op.batchimport.frommanufacturing.notify.ExcelElementNotify
  */
 public class ManufacturerDeviceDataImportOP {
 	///Users/Edmond/gospace
-	public static void main(String[] argv) {//throws ElasticsearchException, ESException, IOException, ParseException{
+	public static void main(String[] argv) throws InterruptedException {//throws ElasticsearchException, ESException, IOException, ParseException{
 		if(argv.length != 1) {
 			System.err.println(String.format("pls input xlsx filepath[%s]", "/BHUData/batchimport/manufacturer/uRouter-20160426-双翼.xlsx"));
 			return;
 		}
 		final String filepath = argv[0];;//"/Users/Edmond/gospace/batchimport/manufacturer/uRouter-20160426-test.xlsx";//argv[0];//
+		
+		System.out.println("xlsx file:"+filepath);
+		Thread.sleep(10*1000);
 		final String Default_orig_vendor = "BHU";
 		final String Default_orig_swver_prefix = "P06V0.0.0Build0000_";
 		//final String filepath = "/BHUData/batchimport/manufacturer/uRouter-20160426-双翼.xlsx";
--- msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/facade/ChargingUnitFacadeService.java
+++ msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/facade/ChargingUnitFacadeService.java
@@ -4,11 +4,14 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.bhu.vas.api.dto.UserType;
 import com.bhu.vas.api.rpc.RpcResponseDTO;
 import com.bhu.vas.api.rpc.RpcResponseDTOBuilder;
 import com.bhu.vas.api.rpc.charging.vto.BatchImportVTO;
+import com.bhu.vas.api.rpc.user.model.User;
 import com.bhu.vas.business.asyn.spring.activemq.service.DeliverMessageService;
 import com.bhu.vas.business.ds.charging.facade.ChargingFacadeService;
+import com.bhu.vas.validate.UserTypeValidateService;
 import com.smartwork.msip.cores.orm.support.page.TailPage;
 import com.smartwork.msip.exception.BusinessI18nCodeException;
 import com.smartwork.msip.jdo.ResponseErrorCode;
@@ -36,6 +39,8 @@ public class ChargingUnitFacadeService {
 			String range_cash_mobile,String range_cash_pc,String access_internet_time,
 			String remark) {
 		try{
+			User operUser = chargingFacadeService.getUserService().getById(uid);
+			UserTypeValidateService.validUserType(operUser, UserType.SelfCmdUser.getSname());
 			return RpcResponseDTOBuilder.builderSuccessRpcResponse(
 					chargingFacadeService.doBatchImportCreate(uid, countrycode, mobileno,
 							sellor,partner,
@@ -54,6 +59,8 @@ public class ChargingUnitFacadeService {
 
 	public RpcResponseDTO<BatchImportVTO> doCancelDeviceRecord(int uid,String batchno) {
 		try{
+			User operUser = chargingFacadeService.getUserService().getById(uid);
+			UserTypeValidateService.validUserType(operUser, UserType.SelfCmdUser.getSname());
 			BatchImportVTO importVTO = chargingFacadeService.doCancelDeviceRecord(uid, batchno);
 			return RpcResponseDTOBuilder.builderSuccessRpcResponse(importVTO);
 		}catch(BusinessI18nCodeException bex){
@@ -66,6 +73,8 @@ public class ChargingUnitFacadeService {
 	
 	public RpcResponseDTO<BatchImportVTO> doConfirmDeviceRecord(int uid,String batchno) {
 		try{
+			User operUser = chargingFacadeService.getUserService().getById(uid);
+			UserTypeValidateService.validUserType(operUser, UserType.SelfCmdUser.getSname());
 			BatchImportVTO importVTO = chargingFacadeService.doConfirmDeviceRecord(uid, batchno);
 			//TODO:async message
 			deliverMessageService.sendBatchImportConfirmActionMessage(uid, batchno);
@@ -81,6 +90,8 @@ public class ChargingUnitFacadeService {
 	public RpcResponseDTO<TailPage<BatchImportVTO>> doPages(int uid,
 			int status, int pageNo, int pageSize) {
 		try{
+			User operUser = chargingFacadeService.getUserService().getById(uid);
+			UserTypeValidateService.validUserType(operUser, UserType.SelfCmdUser.getSname());
 			TailPage<BatchImportVTO> vtos = chargingFacadeService.pagesBatchImport(uid, status,pageNo,pageSize);
 			return RpcResponseDTOBuilder.builderSuccessRpcResponse(vtos);
 		}catch(BusinessI18nCodeException bex){
@@ -96,6 +107,8 @@ public class ChargingUnitFacadeService {
 			String range_cash_mobile, String range_cash_pc,
 			String access_internet_time) {
 		try{
+			//User operUser = chargingFacadeService.getUserService().getById(uid);
+			//UserTypeValidateService.validUserType(operUser, UserType.SelfCmdUser.getSname());
 			deliverMessageService.sendBatchSharedealModifyActionMessage(uid, message, canbeturnoff,enterpriselevel, owner_percent, range_cash_mobile, range_cash_pc, access_internet_time);
 			return RpcResponseDTOBuilder.builderSuccessRpcResponse(Boolean.TRUE);
 		}catch(BusinessI18nCodeException bex){
