68817f7a268057bb406856dcd7821033d8c582b5
Edmond Lee <wenhualee@163.com>
2016-05-13 19:29:44 +0800

    fixed

--- msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/service/device/ChargingRpcService.java
+++ msip-bhu-unit/msip_bhu_unit_vas/src/main/java/com/bhu/vas/rpc/service/device/ChargingRpcService.java
@@ -74,7 +74,7 @@ public class ChargingRpcService  implements IChargingRpcService{
 			String owner_percent,
 			String range_cash_mobile, String range_cash_pc,
 			String access_internet_time) {
-		logger.info(String.format("doBatchSharedealModify uid:%s message:%s canbeturnoff:%s enterpriselevel:%s owner_percent:%s range_cash_mobile:%s range_cash_pc:%s",uid,message, canbeturnoff,enterpriselevel,owner_percent,range_cash_pc, range_cash_pc,access_internet_time));
+		logger.info(String.format("doBatchSharedealModify uid:%s message:%s canbeturnoff:%s enterpriselevel:%s customized:%s owner_percent:%s range_cash_mobile:%s range_cash_pc:%s",uid,message, canbeturnoff,enterpriselevel,customized,owner_percent,range_cash_pc, range_cash_pc,access_internet_time));
 		return chargingUnitFacadeService.doBatchSharedealModify(uid,message, canbeturnoff,enterpriselevel,customized,owner_percent,range_cash_mobile, range_cash_pc,access_internet_time);
 	}
 
