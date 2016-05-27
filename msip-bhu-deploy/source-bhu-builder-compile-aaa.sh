62b2237df7f9c4558f8881e869bd392d530040d9
Edmond Lee <wenhualee@163.com>
2016-05-27 20:32:30 +0800

    fixed

--- msip-bhu-deploy/source-bhu-builder-compile-aaa.sh
+++ msip-bhu-deploy/source-bhu-builder-compile-aaa.sh
@@ -10,6 +10,3 @@ mvn package -Dmaven.test.skip
 echo '代码编译并打包成功'
 #unzip -o test.zip -d tmp/
 #unzip -o msip_bhu_unit_input_processor/bin/msip_bhu_unit_input_processor.jar -d msip_bhu_unit_input_processor/classes/
-
-
-
