fa2037186c1c2d90033cfc010961e0b514240dae
Edmond Lee <wenhualee@163.com>
2016-05-30 19:38:41 +0800

    fixed 增加input consumer logger

--- msip-bhu-unit/msip_bhu_unit_input_processor/src/main/java/com/bhu/vas/processor/input/DeliverMessageTopicConsumer.java
+++ msip-bhu-unit/msip_bhu_unit_input_processor/src/main/java/com/bhu/vas/processor/input/DeliverMessageTopicConsumer.java
@@ -3,19 +3,22 @@ package com.bhu.vas.processor.input;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 
+import com.alibaba.dubbo.common.logger.Logger;
+import com.alibaba.dubbo.common.logger.LoggerFactory;
 import com.bhu.pure.kafka.business.observer.KafkaMsgObserverManager;
 import com.bhu.pure.kafka.client.consumer.StringKafkaMessageConsumer;
 import com.bhu.pure.kafka.client.consumer.callback.PollIteratorNotify;
 
 public class DeliverMessageTopicConsumer extends StringKafkaMessageConsumer{
+	private final Logger logger = LoggerFactory.getLogger(DeliverMessageTopicConsumer.class);
+	private final String LoggerFormatTemplate = "Deliver Recv: topic[%s] partition[%s] key[%s] value[%s] offset[%s] consumerId[%s]";
 	public void init(){
 		super.doSubscribeTopics(new PollIteratorNotify<ConsumerRecords<String,String>>(){
 			@Override
 			public void notifyComming(String consumerId, ConsumerRecords<String, String> records) {
 				for(ConsumerRecord<String, String> record : records){
-					System.out.println(String
-							.format("Received message: topic[%s] partition[%s] key[%s] value[%s] "
-									+ "offset[%s] consumerId[%s]",
+					logger.info(String
+							.format(LoggerFormatTemplate,
 									record.topic(), record.partition(),
 									record.key(), record.value(),
 									record.offset(), consumerId));
