832d04d2f0b86d590387cd02900811a000218ddd
唐子超 <tangzichao@tangzichaodeMacBook-Pro.local>
2016-06-02 09:53:31 +0800

    added

--- msip-bhu-business-impl/msip_bhu_business_push/src/main/java/com/bhu/vas/push/common/context/HandsetOnlineContext.java
+++ msip-bhu-business-impl/msip_bhu_business_push/src/main/java/com/bhu/vas/push/common/context/HandsetOnlineContext.java
@@ -14,6 +14,10 @@ public class HandsetOnlineContext {
 	public static final String Android_Host_Name_Match = "android-";
 	public static final String Android_Terminal = "安卓终端";
 	public static final String Android_Terminal_Unkown = "未知终端";
+	
+	
+	public static final String Ios_HandsetOnline_TextPrefix = "[终端上线]";
+	public static final String Ios_VisitorOnline_TextPrefix = "[访客上线]";
 	//第一次接入设备的终端额外显示名
 	public static final String Android_Stranger = "陌生";
 	//如果用户管理多个设备,需要显示设备信息, 设备信息模板
@@ -21,11 +25,11 @@ public class HandsetOnlineContext {
 	//本次push是否进行推送
 	private boolean vaild;
 	//终端厂家名称
-	private String manufactor = StringHelper.EMPTY_STRING_GAP;
+	//private String manufactor = StringHelper.EMPTY_STRING_GAP;
 	//终端显示名
 	private String handsetName;
 	//陌生显示
-	private String strange = StringHelper.EMPTY_STRING_GAP;
+	//private String strange = StringHelper.EMPTY_STRING_GAP;
 	//设备显示信息
 	private String deviceInfo = StringHelper.EMPTY_STRING_GAP;
 	
@@ -35,12 +39,12 @@ public class HandsetOnlineContext {
 	public void setVaild(boolean vaild) {
 		this.vaild = vaild;
 	}
-	public String getManufactor() {
+/*	public String getManufactor() {
 		return manufactor;
 	}
 	public void setManufactor(String manufactor) {
 		this.manufactor = manufactor;
-	}
+	}*/
 	public String getHandsetName() {
 		return handsetName;
 	}
@@ -51,12 +55,12 @@ public class HandsetOnlineContext {
 	public void setHandsetName(String handsetName) {
 		this.handsetName = handsetName;
 	}
-	public String getStrange() {
+/*	public String getStrange() {
 		return strange;
 	}
 	public void setStrange(String strange) {
 		this.strange = strange;
-	}
+	}*/
 	public String getDeviceInfo() {
 		return deviceInfo;
 	}
