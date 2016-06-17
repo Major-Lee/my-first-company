ef3b8cb2f297d2b7d7f69e0edd5a0caf25127e66
Edmond Lee <wenhualee@163.com>
2016-06-17 19:56:13 +0800

    增加控制用户 提取用户列表类

---
+++ msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/console/ConsoleUserController.java
@@ -0,0 +1,46 @@
+package com.bhu.vas.web.console;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bhu.vas.api.rpc.RpcResponseDTO;
+import com.bhu.vas.api.rpc.user.dto.UserDTO;
+import com.bhu.vas.api.rpc.user.iservice.IUserRpcService;
+import com.bhu.vas.msip.cores.web.mvc.spring.BaseController;
+import com.bhu.vas.msip.cores.web.mvc.spring.helper.SpringMVCHelper;
+import com.smartwork.msip.cores.orm.support.page.TailPage;
+import com.smartwork.msip.jdo.ResponseError;
+import com.smartwork.msip.jdo.ResponseSuccess;
+
+@Controller
+@RequestMapping("/console/user")
+public class ConsoleUserController extends BaseController {
+
+	@Resource
+	private IUserRpcService userRpcService;
+    
+    @ResponseBody()
+    @RequestMapping(value = "/pages", method = {RequestMethod.POST})
+    public void pages(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestParam(required = true) int uid,
+            @RequestParam(required = false) String ut,
+            @RequestParam(required = false, defaultValue = "1", value = "pn") int pageNo,
+			@RequestParam(required = false, defaultValue = "10", value = "ps") int pageSize
+            ) {
+    	RpcResponseDTO<TailPage<UserDTO>> rpcResult = userRpcService.pageUsers(uid, ut, pageNo, pageSize);
+		if(!rpcResult.hasError()){
+			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rpcResult.getPayload()));
+		}else{
+			SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
+		}
+    }
+}
