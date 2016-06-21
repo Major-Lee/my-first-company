716b93e72f6d36abf784ac84cdbc0fcfe52ffe60
JasonDongR <JavaJasonDong@163.com>
2016-06-21 10:46:00 +0800

    fixed

--- msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/console/ConsoleWithdrawController.java
+++ msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/console/ConsoleWithdrawController.java
@@ -149,7 +149,6 @@ public class ConsoleWithdrawController extends BaseController {
     				}else{
     					SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
     				}
-    				//return;
     				continue;
     			}else{
     				RequestWithdrawNotifyDTO withdrawNotify = rpcResult.getPayload();
@@ -159,17 +158,11 @@ public class ConsoleWithdrawController extends BaseController {
         				return;
         			}*/
         			String orderid = withdrawNotify.getOrderid();
-        			System.out.println("*****微信支付：**orderId******"+orderid);
         			String order_amount = String.valueOf(withdrawNotify.getWithdraw().getRealCash());
-        			System.out.println("*****微信支付：**order_amount******"+orderid);
         			String order_transcost = String.valueOf(withdrawNotify.getWithdraw().getTranscost());
-        			System.out.println("*****微信支付：**order_transcost******"+orderid);
         			String order_taxcost = String.valueOf(withdrawNotify.getWithdraw().getTaxcost());
-        			System.out.println("*****微信支付：**order_taxcost******"+orderid);
         			String order_totalamount = String.valueOf(withdrawNotify.getWithdraw().getCash());
-        			System.out.println("*****微信支付：**order_totalamount******"+orderid);
         			String requestIp = WebHelper.getRemoteAddr(request);
-        			System.out.println("*****微信支付：**requestIp******"+orderid);
         			ResponseCreateWithdrawDTO rcp_dto = PaymentInternalHelper.createWithdrawUrlCommunication(
         					withdrawNotify.getAccount().getIdentify(),//.getType(), 
         					orderid,
@@ -182,14 +175,13 @@ public class ConsoleWithdrawController extends BaseController {
         			if(rcp_dto == null){
         				SpringMVCHelper.renderJson(response, ResponseError.embed(RpcResponseDTOBuilder.builderErrorRpcResponse(
         						ResponseErrorCode.INTERNAL_COMMUNICATION_WITHDRAWURL_RESPONSE_INVALID)));
-        				//return;
-        				continue;
+        				return;
+        				//continue;
         			}
         			if(!rcp_dto.isSuccess()){
         				SpringMVCHelper.renderJson(response, ResponseError.embed(RpcResponseDTOBuilder.builderErrorRpcResponse(
         						ResponseErrorCode.INTERNAL_COMMUNICATION_WITHDRAWURL_RESPONSE_FALSE,new String[]{new String(rcp_dto.getMsg())})));
-        				//return;
-        				continue;
+        				return;
         			}
         			System.out.println("apply_payment step 2 from uPay: successed"+JsonHelper.getJSONString(rcp_dto));
         			SpringMVCHelper.renderJson(response, ResponseSuccess.embed(rcp_dto));
@@ -264,9 +256,9 @@ public class ConsoleWithdrawController extends BaseController {
     		}*/
 		}
     	if(!rpcResult.hasError()){
-    		SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
-    	}else{
     		SpringMVCHelper.renderJson(response, ResponseError.SUCCESS);
+    	}else{
+    		SpringMVCHelper.renderJson(response, ResponseError.embed(rpcResult));
     	}
     	return;
     }
