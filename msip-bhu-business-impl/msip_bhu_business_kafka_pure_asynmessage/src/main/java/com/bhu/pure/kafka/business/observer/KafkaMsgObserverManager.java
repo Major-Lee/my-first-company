c3e8861e33e8ad0bb68c3bf48da540dcddf5f82b
Edmond Lee <wenhualee@163.com>
2016-06-21 18:51:52 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_business_kafka_pure_asynmessage/src/main/java/com/bhu/pure/kafka/business/observer/KafkaMsgObserverManager.java
+++ msip-bhu-business-impl/msip_bhu_business_kafka_pure_asynmessage/src/main/java/com/bhu/pure/kafka/business/observer/KafkaMsgObserverManager.java
@@ -3,14 +3,19 @@ package com.bhu.pure.kafka.business.observer;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.bhu.pure.kafka.business.observer.listener.DynaMessageListener;
 
 public class KafkaMsgObserverManager {
+	private static final Logger logger = LoggerFactory.getLogger(KafkaMsgObserverManager.class);
 	private static List<DynaMessageListener> dynaMsgCommingListeners = new CopyOnWriteArrayList<DynaMessageListener>();
 
 	public static class DynaMsgCommingObserver{
 		public static void addMsgCommingListener(DynaMessageListener listener) {
 			dynaMsgCommingListeners.add(listener);
+			logger.info(String.format("DynaMsgComming[%s] Observer register ok", listener.getClass().getSimpleName()));
 	    }
 		
 		public static void removeMsgCommingListener(DynaMessageListener listener) {
@@ -30,6 +35,7 @@ public class KafkaMsgObserverManager {
 	public static class CMNotifyCommingObserver{
 		public static void addMsgCommingListener(DynaMessageListener listener) {
 			cmNotifyCommingListeners.add(listener);
+			logger.info(String.format("CMNotify[%s] Observer register ok", listener.getClass().getSimpleName()));
 	    }
 		
 		public static void removeMsgCommingListener(DynaMessageListener listener) {
