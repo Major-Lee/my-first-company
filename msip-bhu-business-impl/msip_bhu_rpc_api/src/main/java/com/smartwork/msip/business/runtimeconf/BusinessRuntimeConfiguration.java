846f861a1a1f5c57c8a93b09a02f8f54fc77943c
Edmond Lee <wenhualee@163.com>
2016-06-23 09:01:14 +0800

    修改最小提现金额，修改钱包中最少多少零钱的情况下才可以开始提现

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/smartwork/msip/business/runtimeconf/BusinessRuntimeConfiguration.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/smartwork/msip/business/runtimeconf/BusinessRuntimeConfiguration.java
@@ -388,10 +388,10 @@ public class BusinessRuntimeConfiguration extends PropertyResourceConfigurer {
 	public static int CheckArtificialFrdsAddLimitCount = 5;//5个好友数量, 超过数量不在分配客服
 
 	//钱包中有多少零钱之后才能开始提现
-	public static double User_WalletWithdraw_Default_Remainer_MinLimit = 0.10d;//10.00d;
+	public static double User_WalletWithdraw_Default_Remainer_MinLimit = 10.00d;//0.10d;
 	//每次最小提现金额
 	//public static double User_WalletWithdraw_Default_Withdraw_MinLimit = 1.00d;//10.00d;
-	public static double User_WalletWithdraw_Default_Withdraw_MinLimit = 0.10d;//10.00d;
+	public static double User_WalletWithdraw_Default_Withdraw_MinLimit = 10.00d;//0.10d;//
 	public static boolean Device_SharedNetwork_Default_Start = false;
 	public static String Device_SharedNetwork_DUT = "TU";
 	public static String Device_SharedNetwork_Top_Version = "1.5.6";
