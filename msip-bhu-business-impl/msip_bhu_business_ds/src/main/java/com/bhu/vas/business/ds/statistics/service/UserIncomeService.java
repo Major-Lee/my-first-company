92a93574545386c7fd8dad954267f13ad0d17171
Lapaus <Lapaus@163.com>
2016-06-24 17:05:30 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/statistics/service/UserIncomeService.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/statistics/service/UserIncomeService.java
@@ -1,6 +1,8 @@
 package com.bhu.vas.business.ds.statistics.service;
 
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
@@ -8,15 +10,23 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.bhu.vas.api.rpc.charging.model.UserIncome;
 import com.bhu.vas.business.ds.statistics.dao.UserIncomeDao;
-import com.smartwork.msip.business.abstractmsd.service.AbstractCommdityService;
+import com.smartwork.msip.business.abstractmsd.service.AbstractCoreService;
+import com.smartwork.msip.cores.orm.support.criteria.ModelCriteria;
 
 @Service
 @Transactional("coreTransactionManager")
-public class UserIncomeService extends AbstractCommdityService<String, UserIncome, UserIncomeDao>{
+public class UserIncomeService extends AbstractCoreService<String, UserIncome, UserIncomeDao>{
 	@Resource
 	@Override
 	public void setEntityDao(UserIncomeDao userIncomeDao) {
 		super.setEntityDao(userIncomeDao);
 	}
-
+	public List<UserIncome> findListByTime(String time){
+		ModelCriteria mc = new ModelCriteria();
+		mc.createCriteria()
+				.andColumnEqualTo("time", time)
+				.andColumnEqualTo("type", "uid");
+		mc.setOrderByClause("income");
+		return super.findModelByModelCriteria(mc);
+	}
 }
