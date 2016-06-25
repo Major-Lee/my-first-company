ae7be6b13e98e2179ee50988a37614d62c885184
NeeBie-xw <476814114@qq.com>
2016-06-25 10:37:54 +0800

    test

--- msip-bhu-business-impl/msip_bhu_backend_task_statistics/src/main/java/com/bhu/vas/plugins/quartz/StatisticsBackendTaskLoader.java
+++ msip-bhu-business-impl/msip_bhu_backend_task_statistics/src/main/java/com/bhu/vas/plugins/quartz/StatisticsBackendTaskLoader.java
@@ -1,22 +1,7 @@
 package com.bhu.vas.plugins.quartz;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import javax.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.bhu.vas.api.helper.IGenerateDeviceSetting;
-import com.bhu.vas.api.rpc.daemon.iservice.IDaemonRpcService;
-//import com.bhu.vas.business.asyn.spring.builder.ActionMessageFactoryBuilder;
-//import com.bhu.vas.business.asyn.spring.builder.ActionMessageType;
-import com.bhu.vas.business.ds.devicegroup.facade.WifiDeviceGroupFacadeService;
-import com.bhu.vas.business.ds.devicegroup.service.WifiDeviceBackendTaskService;
-import com.bhu.vas.business.ds.task.facade.TaskFacadeService;
-import com.bhu.vas.business.search.service.WifiDeviceDataSearchService;
-import com.bhu.vas.business.search.service.increment.WifiDeviceIndexIncrementProcesser;
 
 /**
  * 此任务暂定5分钟执行一次 根据配置的同时运行的任务数量决定是否需要重新把新的任务加入到任务池中
@@ -27,35 +12,12 @@ import com.bhu.vas.business.search.service.increment.WifiDeviceIndexIncrementPro
 public class StatisticsBackendTaskLoader {
     private static Logger logger = LoggerFactory
 	    .getLogger(StatisticsBackendTaskLoader.class);
-    // 可以放进执行队列的数量
-    // private int poolMax
-    // 可以同时执行的任务的数量
-    // 同时只有三个任务可以执行
-    private ExecutorService task_exec = Executors.newFixedThreadPool(3);
-
-    @Resource
-    private WifiDeviceDataSearchService wifiDeviceDataSearchService;
-
-    @Resource
-    private IDaemonRpcService daemonRpcService;
-
-    @Resource
-    private IGenerateDeviceSetting generateDeviceSetting;
-
-    @Resource
-    private WifiDeviceBackendTaskService wifiDeviceBackendTaskService;
-
-    @Resource
-    private WifiDeviceGroupFacadeService wifiDeviceGroupFacadeService;
-
-    @Resource
-    private TaskFacadeService taskFacadeService;
-
-    @Resource
-    private WifiDeviceIndexIncrementProcesser wifiDeviceIndexIncrementProcesser;
 
     public void execute() throws InterruptedException {
 	logger.info("BusinessStatisticsTaskLoader starting...");
-		
+	logger.info("BusinessStatisticsTaskLoader 111...");
+	logger.info("BusinessStatisticsTaskLoader 222...");
+	logger.info("BusinessStatisticsTaskLoader 333...");
+	logger.info("BusinessStatisticsTaskLoader end...");
     }
 }
