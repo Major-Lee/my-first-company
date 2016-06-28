735044250c5f30dd8272e76d8f80f80536275e2d
Edmond Lee <wenhualee@163.com>
2016-06-28 10:53:37 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_backend_async_online/src/main/java/com/bhu/vas/business/backendonline/asyncprocessor/service/impl/batchsnk/BatchSnkApplyService.java
+++ msip-bhu-business-impl/msip_bhu_backend_async_online/src/main/java/com/bhu/vas/business/backendonline/asyncprocessor/service/impl/batchsnk/BatchSnkApplyService.java
@@ -103,7 +103,8 @@ public class BatchSnkApplyService {
 					 logger.info(String.format("准备开始判定当前信息共享网络状态类型 "));
 					 if(SharedNetworkType.SmsSecure == sharedNetwork){
 						 logger.info(String.format("准备开始判定当前信息共享网络状态类型【%s】【%s】",userid,sharedNetwork.getKey()));
-						 long count = wifiDeviceDataSearchService.searchCountBySnkType(userid,sharedNetwork.getKey(),WifiDeviceDocumentEnumType.SnkTurnStateEnum.Off.getType());
+						 long count = wifiDeviceDataSearchService.searchCountBySnkType(userid,sharedNetwork.getKey(),
+								 WifiDeviceDocumentEnumType.SnkTurnStateEnum.Off.getType());
 						 logger.info(String.format("当前用户信息共享网络状态类型【%s】【%s】 【%s】",userid,sharedNetwork.getKey(),count));
 						 if(count <= 0){//不提供sms认证服务
 							 	logger.info(String.format("当前用户信息共享网络状态类型【%s】【%s】 【%s】开始清除标记",userid,sharedNetwork.getKey(),count));
