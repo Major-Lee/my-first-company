a30ddb08d80872369bd2db5ea15acbaa045b498b
Lapaus <Lapaus@163.com>
2016-06-29 16:40:17 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/dao/UserActivityDao.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/dao/UserActivityDao.java
@@ -3,6 +3,6 @@ package com.bhu.vas.business.ds.user.dao;
 import com.bhu.vas.api.rpc.user.model.UserActivity;
 import com.smartwork.msip.business.abstractmsd.dao.AbstractCoreDao;
 
-public class UserActivityDao extends AbstractCoreDao<String, UserActivity>{
+public class UserActivityDao extends AbstractCoreDao<Integer, UserActivity>{
 
 }
--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/service/UserActivityService.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/service/UserActivityService.java
@@ -4,6 +4,6 @@ import com.bhu.vas.api.rpc.user.model.UserActivity;
 import com.bhu.vas.business.ds.user.dao.UserActivityDao;
 import com.smartwork.msip.business.abstractmsd.service.AbstractCoreService;
 
-public class UserActivityService extends AbstractCoreService<String,UserActivity, UserActivityDao>{
+public class UserActivityService extends AbstractCoreService<Integer,UserActivity, UserActivityDao>{
 
 }
