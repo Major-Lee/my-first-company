4b18319289d2c7ab7b5ea9f21a7f7a9302ed986c
Edmond Lee <wenhualee@163.com>
2016-07-02 12:21:31 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/service/UserActivityService.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/service/UserActivityService.java
@@ -1,9 +1,13 @@
 package com.bhu.vas.business.ds.user.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.bhu.vas.api.rpc.user.model.UserActivity;
 import com.bhu.vas.business.ds.user.dao.UserActivityDao;
 import com.smartwork.msip.business.abstractmsd.service.AbstractCoreService;
-
+@Service
+@Transactional("coreTransactionManager")
 public class UserActivityService extends AbstractCoreService<Integer,UserActivity, UserActivityDao>{
 
 }
