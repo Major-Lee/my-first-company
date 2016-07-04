4e8e87a0f837f7f5a89b101e356db48c878cfe1d
JasonDongR <JavaJasonDong@163.com>
2016-07-04 17:27:28 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/dao/UserActivityDao.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/dao/UserActivityDao.java
@@ -1,8 +1,10 @@
 package com.bhu.vas.business.ds.user.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.bhu.vas.api.rpc.user.model.UserActivity;
 import com.smartwork.msip.business.abstractmsd.dao.AbstractCoreDao;
-
+@Repository
 public class UserActivityDao extends AbstractCoreDao<Integer, UserActivity>{
 
 }
