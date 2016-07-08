58acfa9e08a891fe627a9fce159c4cec5e028ecf
Lapaus <Lapaus@163.com>
2016-07-08 10:20:17 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/statistics/dao/UserIncomeDao.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/statistics/dao/UserIncomeDao.java
@@ -7,6 +7,12 @@ import com.smartwork.msip.business.abstractmsd.dao.AbstractCoreDao;
 @Repository
 public class UserIncomeDao extends AbstractCoreDao<String, UserIncome>{
 	public double countIncome(int uid) {
-		return super.getSqlSessionMasterTemplate().selectOne(UserIncome.class.getName()+".countIncome",uid);
+		double income=0;
+		try {
+			income=super.getSqlSessionMasterTemplate().selectOne(UserIncome.class.getName()+".countIncome",uid);
+		} catch (Exception e) {
+			return income;
+		}
+		return income;
 	}
 }
