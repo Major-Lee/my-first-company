8c1ea25c2799a361f4e9cf896c2a8e647e010234
reid <pengyuzpy@gmail.com>
2016-07-12 13:31:29 +0800

    add 终端肖像

--- msip-bhu-business-impl/msip_bhu_business_ds_portrait/src/test/java/com/bhu/vas/business/user/HandsetPortraitServiceTest.java
+++ msip-bhu-business-impl/msip_bhu_business_ds_portrait/src/test/java/com/bhu/vas/business/user/HandsetPortraitServiceTest.java
@@ -1,13 +1,22 @@
 package com.bhu.vas.business.user;
+import java.util.Date;
+
 import javax.annotation.Resource;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import com.bhu.vas.api.rpc.portrait.model.HandsetPortrait;
 import com.bhu.vas.business.portrait.ds.hportrait.service.HandsetPortraitService;
 import com.smartwork.msip.localunit.BaseTest;
+
+import eu.bitwalker.useragentutils.Browser;
+import eu.bitwalker.useragentutils.OperatingSystem;
+import eu.bitwalker.useragentutils.UserAgent;
+import eu.bitwalker.useragentutils.Version;
 
 /**
  * 主键索引
@@ -33,4 +42,40 @@ public class HandsetPortraitServiceTest extends BaseTest{
     	Thread.sleep(1000);
     }
 
+    @Test
+	public void test001BatchCreateWithdraw() {
+//		String userAgents = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+//		String userAgents = "User-Agent: Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
+		String userAgents = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0); 360Spider";
+		UserAgent userAgent = UserAgent.parseUserAgentString(userAgents);  
+		Browser browser = userAgent.getBrowser(); 
+		Version browserVersion = userAgent.getBrowserVersion();
+		OperatingSystem os = userAgent.getOperatingSystem();
+		System.out.println("browserVersion:"+browserVersion.getVersion());
+		System.out.println("browserName:"+browser.getName());
+		System.out.println("browserType:"+browser.getBrowserType());
+		System.out.println("browser ManufacturerName:"+browser.getManufacturer());
+		System.out.println("browser RenderingEngine:"+browser.getRenderingEngine());
+		System.out.println("OS name:"+ os.getName());
+		System.out.println("OS isMobileDevice:"+os.isMobileDevice());
+		System.out.println("OS ID:"+os.getId());
+		System.out.println("OS DeviceType:"+os.getDeviceType());
+		System.out.println("OS Manufacturer:"+os.getManufacturer());
+		HandsetPortrait ua = new HandsetPortrait();
+		ua.setBrowserType(browser.getBrowserType().toString());
+		ua.setBrowserManufacturer(browser.getManufacturer().toString());
+		ua.setBrowserVersion(browserVersion.getVersion());
+		ua.setId("40-F0-2F-3A-A3-64");
+		ua.setMobile("18640801793");
+		ua.setOsManufacturer(os.getManufacturer().toString());
+		ua.setOsType(os.getDeviceType().toString());
+		boolean isMobileType = os.isMobileDevice();
+		if(isMobileType){
+			ua.setType(0);
+		}else{
+			ua.setType(1);
+		}
+		ua.setCreated_at(new Date());
+		handsetPortraitService.insert(ua);
+	}
 }
--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/portrait/model/HandsetPortrait.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/portrait/model/HandsetPortrait.java
@@ -1,22 +1,68 @@
 package com.bhu.vas.api.rpc.portrait.model;
 
-import com.smartwork.msip.cores.orm.model.BaseStringModel;
-@SuppressWarnings("serial")
-public class HandsetPortrait extends BaseStringModel{// implements ISequenceGenable,TableSplitable<Integer>{
-	
-	private String mobileno;
-	private String os;
-	public String getMobileno() {
-		return mobileno;
-	}
-	public void setMobileno(String mobileno) {
-		this.mobileno = mobileno;
-	}
-	public String getOs() {
-		return os;
-	}
-	public void setOs(String os) {
-		this.os = os;
-	}
+import java.util.Date;
 
+import com.smartwork.msip.cores.orm.model.BaseStringModel;
+
+@SuppressWarnings("serial")
+public class HandsetPortrait  extends BaseStringModel{
+	
+	private String mobile;
+	private String browserVersion;
+	private String browserType;
+	private String browserManufacturer;
+	private String osType;
+	private String osManufacturer;
+	private int type;
+	private Date created_at;
+	
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getBrowserVersion() {
+		return browserVersion;
+	}
+	public void setBrowserVersion(String browserVersion) {
+		this.browserVersion = browserVersion;
+	}
+	public String getBrowserType() {
+		return browserType;
+	}
+	public void setBrowserType(String browserType) {
+		this.browserType = browserType;
+	}
+	public String getBrowserManufacturer() {
+		return browserManufacturer;
+	}
+	public void setBrowserManufacturer(String browserManufacturer) {
+		this.browserManufacturer = browserManufacturer;
+	}
+	public String getOsType() {
+		return osType;
+	}
+	public void setOsType(String osType) {
+		this.osType = osType;
+	}
+	public String getOsManufacturer() {
+		return osManufacturer;
+	}
+	public void setOsManufacturer(String osManufacturer) {
+		this.osManufacturer = osManufacturer;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public Date getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+	
 }
