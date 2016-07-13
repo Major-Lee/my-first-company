c7df4697b74ecd3d0133a736109f792769094378
唐子超 <tangzichao@tangzichaodeMacBook-Pro.local>
2016-07-13 14:32:33 +0800

    added

--- msip-bhu-unit/msip_bhu_unit_index/src/main/java/com/bhu/vas/business/index/processor/BusinessIncrementReceivePerformMessageProcessor.java
+++ msip-bhu-unit/msip_bhu_unit_index/src/main/java/com/bhu/vas/business/index/processor/BusinessIncrementReceivePerformMessageProcessor.java
@@ -49,9 +49,9 @@ public class BusinessIncrementReceivePerformMessageProcessor implements DynaMess
 			ExecutorService exec_process = ExecObserverManager.buildExecutorService(this.getClass(),"DynaMsg process消息处理".concat(String.valueOf(i)),per_threads);
 			exec_processes.add(exec_process);//Executors.newFixedThreadPool(per_threads));
 		}*/
-		exec_dispatcher = ExecObserverManager.buildSingleThreadExecutor(this.getClass(),"DynaMsg dispatcher消息处理");
+		exec_dispatcher = ExecObserverManager.buildSingleThreadExecutor(this.getClass(),"Perform dispatcher消息处理");
 		for(int i=0;i<hash_prime;i++){
-			ExecutorService exec_process = ExecObserverManager.buildSingleThreadExecutor(this.getClass(),"DynaMsg process消息处理".concat(String.valueOf(i)));
+			ExecutorService exec_process = ExecObserverManager.buildSingleThreadExecutor(this.getClass(),"Perform process消息处理".concat(String.valueOf(i)));
 			exec_processes.add(exec_process);//Executors.newFixedThreadPool(per_threads));
 		}
 		
