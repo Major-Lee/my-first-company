16621e057a3735b2c8001410ce26b6f2877a2333
NeeBie-xw <476814114@qq.com>
2016-07-19 15:20:38 +0800

    fixed

--- msip-bhu-unit/msip_bhu_unit_uass/src/main/java/com/bhu/vas/rpc/facade/UserUnitFacadeService.java
+++ msip-bhu-unit/msip_bhu_unit_uass/src/main/java/com/bhu/vas/rpc/facade/UserUnitFacadeService.java
@@ -16,7 +16,6 @@ import com.bhu.vas.api.dto.UserType;
 import com.bhu.vas.api.helper.WifiDeviceDocumentEnumType.OnlineEnum;
 import com.bhu.vas.api.rpc.RpcResponseDTO;
 import com.bhu.vas.api.rpc.RpcResponseDTOBuilder;
-import com.bhu.vas.api.rpc.UserSettingStateService;
 import com.bhu.vas.api.rpc.charging.model.UserIncome;
 import com.bhu.vas.api.rpc.user.dto.UserConfigsStateDTO;
 import com.bhu.vas.api.rpc.user.dto.UserDTO;
@@ -191,17 +190,6 @@ public class UserUnitFacadeService {
 		UserInnerExchangeDTO userExchange = userSignInOrOnFacadeService.commonUserValidate(user,uToken, device, remoteIp,d_udid);
 		Map<String, Object> rpcPayload = RpcResponseDTOBuilder.builderUserRpcPayload(
 				userExchange);//,userWifiDeviceFacadeService.fetchBindDevices(userExchange.getUser().getId()));
-		//add by fengshibo for push switch
-		UserConfigsState userConfigsState = userConfigsStateService.getById(userExchange.getUser().getId());
-		if (userConfigsState != null){
-			UserConfigsStateDTO dto = JsonHelper.getDTO(userConfigsState.getExtension_content(), UserConfigsStateDTO.class);
-			if (dto!= null) {
-				rpcPayload.put("rn_on", dto.isRn_on());
-			}else{
-				rpcPayload.put("rn_on", Boolean.TRUE);
-			}
-		}
-		
 		
 		deliverMessageService.sendUserSignedonActionMessage(user.getId(), remoteIp,device);
 		return RpcResponseDTOBuilder.builderSuccessRpcResponse(rpcPayload);
@@ -522,6 +510,16 @@ public class UserUnitFacadeService {
 			//userExchange.setWallet(userWalletFacadeService.walletDetail(uid));
 			//userExchange.setOauths(userOAuthFacadeService.fetchRegisterIdentifies(userExchange.getUser().getId(),false));
 			Map<String, Object> rpcPayload = RpcResponseDTOBuilder.builderUserRpcPayload(userExchange);
+			//add by fengshibo for push switch
+			UserConfigsState userConfigsState = userConfigsStateService.getById(userExchange.getUser().getId());
+			if (userConfigsState != null){
+				UserConfigsStateDTO dto = JsonHelper.getDTO(userConfigsState.getExtension_content(), UserConfigsStateDTO.class);
+				if (dto!= null) {
+					rpcPayload.put("rn_on", dto.isRn_on());
+				}else{
+					rpcPayload.put("rn_on", Boolean.TRUE);
+				}
+			}
 			return RpcResponseDTOBuilder.builderSuccessRpcResponse(rpcPayload);
 		}catch(BusinessI18nCodeException bex){
 			return RpcResponseDTOBuilder.builderErrorRpcResponse(bex.getErrorCode(),bex.getPayload());
