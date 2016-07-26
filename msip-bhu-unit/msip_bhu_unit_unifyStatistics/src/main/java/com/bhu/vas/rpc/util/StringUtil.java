96c5c3a82a818a15c31847702230034586067d5c
Lapaus <Lapaus@163.com>
2016-07-26 09:50:57 +0800

    fixed

--- msip-bhu-unit/msip_bhu_unit_unifyStatistics/src/main/java/com/bhu/vas/rpc/util/StringUtil.java
+++ msip-bhu-unit/msip_bhu_unit_unifyStatistics/src/main/java/com/bhu/vas/rpc/util/StringUtil.java
@@ -1868,26 +1868,6 @@ public class StringUtil {
 		return "";
 	}
 
-	/**
-	 * 判断某个字符串是否存在于数组中
-	 * 
-	 * @param stringArray
-	 *            原数组
-	 * @param source
-	 *            查找的字符串
-	 * @return 是否找到
-	 */
-	public static boolean contains(String[] stringArray, String source) {
-		// 转换为list
-		List<String> tempList = Arrays.asList(stringArray);
-
-		// 利用list的包含方法,进行判断
-		if (oConvertUtils.isEmpty(source)|| tempList.contains(source)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
 
 	/**
 	 * html 必须是格式良好的
