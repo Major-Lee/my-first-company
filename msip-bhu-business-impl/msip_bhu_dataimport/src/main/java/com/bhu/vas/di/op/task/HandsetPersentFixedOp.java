5a88c0655922a2826a9552b2a674e8d0cca8a34a
NeeBie-xw <476814114@qq.com>
2016-07-28 12:00:39 +0800

    fixed

---
+++ msip-bhu-business-impl/msip_bhu_dataimport/src/main/java/com/bhu/vas/di/op/task/HandsetPersentFixedOp.java
@@ -0,0 +1,37 @@
+package com.bhu.vas.di.op.task;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import com.bhu.vas.business.bucache.redis.serviceimpl.devices.WifiDeviceHandsetUnitPresentSortedSetService;
+import com.smartwork.msip.cores.helper.DateTimeHelper;
+
+import redis.clients.jedis.Tuple;
+
+public class HandsetPersentFixedOp {
+	public static void main(String[] args) {
+		System.out.println("HandsetPersentFixedOp start...");
+		Date date = DateTimeHelper.getDateDaysAgo(3);
+		long lastDate = date.getTime();
+		double score = WifiDeviceHandsetUnitPresentSortedSetService.getInstance().generateScore(lastDate);
+		int amount = 0;
+		Set<String> redisKeys = WifiDeviceHandsetUnitPresentSortedSetService.getInstance().keySet();
+		for(String key : redisKeys){
+			List<String> wasteData = new ArrayList<String>();
+			Set<Tuple> handsets = WifiDeviceHandsetUnitPresentSortedSetService.getInstance().fetchOfflinePresentWithScores(key);
+			if (handsets != null && handsets.isEmpty()) {
+				for (Tuple tuple : handsets) {
+					if (score > tuple.getScore()) {
+						wasteData.add(tuple.getElement());
+						System.out.println(String.format("HandsetPersentFixedOp need clean handset[%s]", tuple.getElement()));
+					}
+				}
+			 	WifiDeviceHandsetUnitPresentSortedSetService.getInstance().removePresentsWithKey(key, wasteData);
+			}
+			amount += wasteData.size();
+		}
+		System.out.println(String.format("HandsetPersentFixedOp end... total[%s]", amount));
+	}
+}
