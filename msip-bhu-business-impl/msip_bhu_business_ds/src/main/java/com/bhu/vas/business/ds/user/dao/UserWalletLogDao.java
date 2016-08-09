9b7e1f4e04bf3700645ef6a83c75ed45b78584d2
fengshibo <fengshibo610524@163.com>
2016-08-09 17:00:09 +0800

    增加walletlog表sumandcount查询

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/dao/UserWalletLogDao.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/user/dao/UserWalletLogDao.java
@@ -1,5 +1,8 @@
 package com.bhu.vas.business.ds.user.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
 
 import com.bhu.vas.api.rpc.user.model.UserWalletLog;
@@ -8,5 +11,19 @@ import com.smartwork.msip.business.abstractmsd.dao.AbstractCoreDao;
 
 @Repository
 public class UserWalletLogDao extends AbstractCoreDao<Long,UserWalletLog>{
-	
+	public Map<String,Object> fetchCashSumAndCountByUid(Integer uid,String start_time,String end_time,String mac){
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("uid", uid);
+		map.put("start_time", start_time);
+		map.put("end_time", end_time);
+		if (!mac.isEmpty() || mac != "")
+			map.put("mac", mac);
+		Map<String,Object> result = new HashMap<String,Object>();
+		try{
+			result = super.getSqlSessionMasterTemplate().selectOne(UserWalletLog.class.getName()+".countAndsum", map);
+		}catch (Exception e) {
+			return result;
+		}
+		return result;
+	}
 }
