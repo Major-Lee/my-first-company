464e4676b36717ddc47b821dce17fe4157fe2d34
reid <pengyuzpy@gmail.com>
2016-08-11 16:24:32 +0800

    add Wang An

---
+++ msip-bhu-unit/msip_bhu_unit_wangan_processor/src/main/java/com/bhu/vas/processor/bulogs/TerminalStatusNotifyLogger.java
@@ -0,0 +1,24 @@
+package com.bhu.vas.processor.bulogs;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * 通知设备打印日志
+ * @author PengYu Zhang
+ *
+ */
+public class TerminalStatusNotifyLogger {
+	private static final Logger logger = LoggerFactory.getLogger(TerminalStatusNotifyLogger.class);
+	private static final String Terminal_Status_Notify_Logger_Template = "%s";
+	public static void doTerminalStatusMessageLog(String messagejson){
+		logger.info(String.format(Terminal_Status_Notify_Logger_Template, messagejson));
+	}
+	      
+	
+	public static void main(String[] argv){  
+		TerminalStatusNotifyLogger.doTerminalStatusMessageLog("qqqqqqqqqqqqqqq");
+		//BusinessStatisticsLogger.doSignedLog(100097, System.currentTimeMillis(), System.currentTimeMillis());
+		//BusinessStatisticsLogger.doSnsShareLog(100097, "qq",200345,"分享了。。。", System.currentTimeMillis());
+	}
+}
