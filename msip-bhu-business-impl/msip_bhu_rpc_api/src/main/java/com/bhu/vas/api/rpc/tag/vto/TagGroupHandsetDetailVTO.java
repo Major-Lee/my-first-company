ad06541540920c745ed88b0a3af833690b8a1b98
李晓玮 <476814114@qq.com>
2016-09-09 09:14:30 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/tag/vto/TagGroupHandsetDetailVTO.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/rpc/tag/vto/TagGroupHandsetDetailVTO.java
@@ -2,7 +2,6 @@ package com.bhu.vas.api.rpc.tag.vto;
 
 import java.io.Serializable;
 import java.util.Map;
-import com.smartwork.msip.cores.plugins.dictparser.impl.mac.MacDictParserFilterHelper;
 
 @SuppressWarnings("serial")
 public class TagGroupHandsetDetailVTO implements Serializable{
@@ -55,7 +54,7 @@ public class TagGroupHandsetDetailVTO implements Serializable{
 		vto.setHdMac((String)map.get("hdmac"));
 		vto.setMobileno((String)map.get("mobileno"));
 		vto.setCount((Long)map.get("count"));
-		vto.setManu(MacDictParserFilterHelper.prefixMactch(vto.getHdMac(),true,false));
+//		vto.setManu(MacDictParserFilterHelper.prefixMactch(vto.getHdMac(),true,false));
 		vto.setFirstTime((String)map.get("min"));
 		vto.setLastTime((String)map.get("max"));
 		return vto;
