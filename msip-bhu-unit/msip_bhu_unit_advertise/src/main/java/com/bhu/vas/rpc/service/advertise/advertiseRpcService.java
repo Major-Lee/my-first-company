d5058bd0d49926ef94e6ea0990558e5f0f54cce9
李晓玮 <476814114@qq.com>
2016-10-18 13:52:33 +0800

    fixed

--- msip-bhu-unit/msip_bhu_unit_advertise/src/main/java/com/bhu/vas/rpc/service/advertise/advertiseRpcService.java
+++ msip-bhu-unit/msip_bhu_unit_advertise/src/main/java/com/bhu/vas/rpc/service/advertise/advertiseRpcService.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Service;
 
 import com.alibaba.dubbo.common.logger.Logger;
 import com.alibaba.dubbo.common.logger.LoggerFactory;
+import com.bhu.vas.api.rpc.advertise.iservice.IAdvertiseRpcService;
 
 @Service("advertiseRpcService")
-public class advertiseRpcService{
+public class advertiseRpcService implements IAdvertiseRpcService{
 	private final Logger logger = LoggerFactory.getLogger(advertiseRpcService.class);
 }
