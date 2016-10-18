b3f2ab21c80c0cca00a7c1d03dde9e06738d76b1
李晓玮 <476814114@qq.com>
2016-10-18 11:26:03 +0800

    ad controller

---
+++ msip-bhu-api/msip_bhu_rest/src/main/java/com/bhu/vas/web/advertise/AdvertiseController.java
@@ -0,0 +1,12 @@
+package com.bhu.vas.web.advertise;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bhu.vas.msip.cores.web.mvc.spring.BaseController;
+
+@Controller
+@RequestMapping("/ad")
+public class AdvertiseController extends BaseController{
+
+}
