d52b68a922bcb848ecd9b2030d0453cd97cf1658
Lapaus <Lapaus@163.com>
2016-10-27 17:53:05 +0800

    fixed 区域限制

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/advertise/dao/AdvertiseDao.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/advertise/dao/AdvertiseDao.java
@@ -10,11 +10,15 @@ import com.bhu.vas.api.rpc.advertise.model.Advertise;
 import com.smartwork.msip.business.abstractmsd.dao.AbstractCoreDao;
 @Repository
 public class AdvertiseDao extends AbstractCoreDao<Integer, Advertise>{
-	public int countByAdvertiseTime(Date start,Date end){
+	public int countByAdvertiseTime(Date start,Date end,String province, String city,
+			String district ){
 		int n=0;
 		Map<String,Object> map = new HashMap<String,Object>();
 		map.put("start", start);
 		map.put("end", end);
+		map.put("province", province);
+		map.put("city", city);
+		map.put("district", district);
 		try {
 			n = super.getSqlSessionMasterTemplate().selectOne(Advertise.class.getName()+".countByAdvertiseTime",map);
 		} catch (Exception e) {
