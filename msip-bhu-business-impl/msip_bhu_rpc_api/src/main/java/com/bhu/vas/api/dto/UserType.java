543b2274e0b18865a7cd93d55cc37e6cf77185c8
李晓玮 <476814114@qq.com>
2016-11-01 14:36:30 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/dto/UserType.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/dto/UserType.java
@@ -25,7 +25,7 @@ public enum UserType {
 	BusinessSelfor(42,"BS","自运营的商户"),
 	//add by Jason 2016-07-11 start
 	//城市运营商
-	URBANOPERATORS(43,"UP","城市运营商"),
+	URBANOPERATORS(43,"UP","渠道运营商"),
 	//add by Jason 2016-07-11 start
 	//Comprehensive management department
 	SelfCmdUser(50,"S","我司综合管理部门用户"),
