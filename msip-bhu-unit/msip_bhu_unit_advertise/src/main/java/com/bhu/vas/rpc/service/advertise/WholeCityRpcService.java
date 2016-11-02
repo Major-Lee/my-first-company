09300159716834da8ee60b4573dc523b2cc4c637
Lapaus <Lapaus@163.com>
2016-11-02 16:07:27 +0800

    fixed

--- msip-bhu-unit/msip_bhu_unit_advertise/src/main/java/com/bhu/vas/rpc/service/advertise/WholeCityRpcService.java
+++ msip-bhu-unit/msip_bhu_unit_advertise/src/main/java/com/bhu/vas/rpc/service/advertise/WholeCityRpcService.java
@@ -53,7 +53,7 @@ public class WholeCityRpcService implements IAdvertiseRpcService{
 			String url,String domain, String province, String city, String district,String description,String title,
 			long start, long end) {
 		logger.info(String.format("createNewAdvertise with uid[%s] image[%s] url[%s] domain[%s] province[%s] city[%s] district[%s] title[%s] description[%s] start[%s] start[%s]",
-				uid, image, url,domain,title, province, city, district,description, start, end));
+				uid, image, url,domain,province, city, district,title,description, start, end));
 		return advertiseUnitFacadeService.createNewAdvertise(uid, image, url,domain, province, city, district,description,title, start, end);
 	}
 
