87ba99e382d628dd813fc56f928e4521406d9cd5
Lapaus <Lapaus@163.com>
2016-11-04 11:23:30 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/advertise/service/AdvertiseService.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/advertise/service/AdvertiseService.java
@@ -6,14 +6,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bhu.vas.api.rpc.advertise.model.Advertise;
+import com.bhu.vas.business.bucache.redis.serviceimpl.unique.SequenceService;
 import com.bhu.vas.business.ds.advertise.dao.AdvertiseDao;
-import com.smartwork.msip.business.abstractmsd.service.AbstractCoreService;
+import com.smartwork.msip.business.abstractmsd.service.AbstractCommdityService;
 @Service
 @Transactional("coreTransactionManager")
-public class AdvertiseService extends AbstractCoreService<String, Advertise, AdvertiseDao>{
+public class AdvertiseService extends AbstractCommdityService<String, Advertise, AdvertiseDao>{
 	@Resource
 	@Override
 	public void setEntityDao(AdvertiseDao advertiseDao) {
 		super.setEntityDao(advertiseDao);
+	}
+	
+	@Override
+	public Advertise insert(Advertise entity) {
+		//如果订单id为空 会默认生成订单id 并且扩展占位为零
+		if(entity.getId() == null){
+			SequenceService.getInstance().onCreateSequenceKey(entity, false);
+		}
+		return super.insert(entity);
 	}
 }
