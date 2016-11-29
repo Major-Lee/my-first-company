6f672514f3bf7d0e2df93e4ba8155d4a3375fb78
李晓玮 <476814114@qq.com>
2016-11-29 10:33:34 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/advertise/AdvertiseBillsVTO.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/vto/advertise/AdvertiseBillsVTO.java
@@ -1,5 +1,7 @@
 package com.bhu.vas.api.vto.advertise;
 
+import java.math.BigDecimal;
+
 @SuppressWarnings("serial")
 public class AdvertiseBillsVTO implements java.io.Serializable{
 	private float expect;
@@ -9,19 +11,24 @@ public class AdvertiseBillsVTO implements java.io.Serializable{
 	public float getExpect() {
 		return expect;
 	}
-	public void setExpect(float string) {
-		this.expect = string;
+	public void setExpect(float expect) {
+		this.expect = cutDecimal(expect);
 	}
 	public float getActual() {
 		return actual;
 	}
 	public void setActual(float actual) {
-		this.actual = actual;
+		this.actual = cutDecimal(actual);
 	}
 	public float getBalance() {
 		return balance;
 	}
 	public void setBalance(float balance) {
-		this.balance = balance;
+		this.balance = cutDecimal(balance);
+	}
+	
+	private float cutDecimal (float f){
+		 BigDecimal   b  =   new BigDecimal(f);  
+		 return b.setScale(2, BigDecimal.ROUND_DOWN).floatValue();  
 	}
 }
