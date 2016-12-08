6396b5ae78cfca08aa790bd9dc26efdd8a4c8dcb
Lapaus <Lapaus@163.com>
2016-12-08 19:44:28 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/dto/procedure/ShareDealWalletSummaryProcedureDTO.java
+++ msip-bhu-business-impl/msip_bhu_rpc_api/src/main/java/com/bhu/vas/api/dto/procedure/ShareDealWalletSummaryProcedureDTO.java
@@ -167,14 +167,12 @@ public class ShareDealWalletSummaryProcedureDTO extends AbstractProcedureDTO{
 	
 	public ShareDealWalletSummaryProcedureVTO toVTO(){
 		ShareDealWalletSummaryProcedureVTO vto = new ShareDealWalletSummaryProcedureVTO();
-		vto.setUserid(this.getUserid());
+			vto.setUserid(this.getUserid());
+			//boolean isTheToday = (this.getLast_update_cash_datetime() == null) ? false : 
+				//DateTimeHelper.isSameDay(this.getLast_update_cash_datetime(), new Date());
+		vto.setToday_cash(Float.valueOf(ArithHelper.getCuttedCurrency(this.getToday_cash() + "")));
 		vto.setOds(this.getOds());
 		vto.setToday_date(this.getToday_date());
-	    
-		boolean isTheToday = (this.getLast_update_cash_datetime() == null) ? false : 
-			DateTimeHelper.isSameDay(this.getLast_update_cash_datetime(), new Date());
-		
-		vto.setToday_cash(isTheToday ? Float.valueOf(ArithHelper.getCuttedCurrency(this.getToday_cash() + "")) : 0);
 		vto.setToday_nums(this.getToday_nums());
 		vto.setYesterday_date(this.getYesterday_date());
 		vto.setYesterday_cash(Float.valueOf(ArithHelper.getCuttedCurrency(this.getYesterday_cash() + "")));
