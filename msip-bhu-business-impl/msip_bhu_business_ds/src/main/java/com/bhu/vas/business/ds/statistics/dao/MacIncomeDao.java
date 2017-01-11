423f93c7d394f559b66a726caf82abd474f992dc
pengyuzpy <pengyuzpy@gmail.com>
2017-01-11 14:35:31 +0800

    fixed

--- msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/statistics/dao/MacIncomeDao.java
+++ msip-bhu-business-impl/msip_bhu_business_ds/src/main/java/com/bhu/vas/business/ds/statistics/dao/MacIncomeDao.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import com.bhu.vas.api.rpc.charging.model.MacIncome;
-import com.bhu.vas.api.rpc.charging.model.UserIncome;
 import com.smartwork.msip.business.abstractmsd.dao.AbstractCoreDao;
 @Repository
 public class MacIncomeDao extends AbstractCoreDao<Integer, MacIncome>{
