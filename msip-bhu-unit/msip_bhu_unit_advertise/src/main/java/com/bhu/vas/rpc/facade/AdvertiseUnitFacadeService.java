9b345a16ca898e3c57e9dd296e834fa6b60f9dc3
李晓玮 <476814114@qq.com>
2017-01-18 14:02:20 +0800

    fixed

--- msip-bhu-unit/msip_bhu_unit_advertise/src/main/java/com/bhu/vas/rpc/facade/AdvertiseUnitFacadeService.java
+++ msip-bhu-unit/msip_bhu_unit_advertise/src/main/java/com/bhu/vas/rpc/facade/AdvertiseUnitFacadeService.java
@@ -759,7 +759,11 @@ public class AdvertiseUnitFacadeService {
 								vto.setDomain(doc.getA_domain());
 								vto.setImage(doc.getA_image());
 								vto.setExtparams(doc.getA_extparams());
-								extparams.add(doc.getA_extparams());
+								if(doc.getA_extparams() != null){
+									extparams.add(doc.getA_extparams());
+								}else{
+									extparams.add("nothing");
+								}
 								vto.setReject_reason(doc.getA_reject_reason());
 								vtos.add(vto);
 							}
